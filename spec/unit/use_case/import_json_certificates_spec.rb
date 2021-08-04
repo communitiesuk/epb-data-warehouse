38ce2a9d42dcd2703404f39a7a4c0d1ac8499ceb
Aga Dufrat <a.dufrat@gmail.com>
2021-08-04 08:21:08 +0100

    Correct RSpec offences

--- spec/unit/use_case/import_json_certificates_spec.rb
+++ spec/unit/use_case/import_json_certificates_spec.rb
@@ -1,99 +1,97 @@
 describe UseCase::ImportJsonCertificates do
-  context "call import use case with mocked gateway" do
-    subject do
-      described_class.new(directory_gateway, assessment_attribute_gateway)
-    end
+  subject do
+    described_class.new(directory_gateway, assessment_attribute_gateway)
+  end
 
-    let(:directory_gateway) { instance_double(Gateway::JsonCertificates) }
+  let(:directory_gateway) { instance_double(Gateway::JsonCertificates) }
 
-    let(:assessment_attribute_gateway) do
-      instance_double(Gateway::AssessmentAttributesGateway)
-    end
+  let(:assessment_attribute_gateway) do
+    instance_double(Gateway::AssessmentAttributesGateway)
+  end
 
-    let!(:files) do
-      path = File.join Dir.pwd, "spec/fixtures/json_export/"
-      Dir
-        .glob(File.join(path, "**", "*"))
-        .select { |file| File.file?(file) && File.extname(file) == ".json" }
-    end
+  let!(:files) do
+    path = File.join Dir.pwd, "spec/fixtures/json_export/"
+    Dir
+      .glob(File.join(path, "**", "*"))
+      .select { |file| File.file?(file) && File.extname(file) == ".json" }
+  end
 
+  before do
+    allow(directory_gateway).to receive(:read).and_return(files)
+    allow(assessment_attribute_gateway).to receive(:add_attribute_value)
+                                             .and_return(1)
+  end
+
+  it "imports 3 files from the fixtures" do
+    expect(files.count).to eq(3)
+  end
+
+  context "when use case uses the actual attribute gateway" do
     before do
-      allow(directory_gateway).to receive(:read).and_return(files)
-      allow(assessment_attribute_gateway).to receive(:add_attribute_value)
-                                               .and_return(1)
+      use_case =
+        described_class.new(
+          directory_gateway,
+          Gateway::AssessmentAttributesGateway.new,
+        )
+      use_case.execute
     end
 
-    it "imports 3 files from the fixtures" do
-      expect(files.count).to eq(3)
-    end
+    let!(:imported_data) do
+      use_case =
+        described_class.new(
+          directory_gateway,
+          Gateway::AssessmentAttributesGateway.new,
+        )
+      use_case.execute
 
-    context "when use case uses the actual attribute gateway" do
-      before do
-        use_case =
-          described_class.new(
-            directory_gateway,
-            Gateway::AssessmentAttributesGateway.new,
-          )
-        use_case.execute
-      end
-
-      let!(:imported_data) do
-        use_case =
-          described_class.new(
-            directory_gateway,
-            Gateway::AssessmentAttributesGateway.new,
-          )
-        use_case.execute
-
-        ActiveRecord::Base.connection.exec_query(
-          'SELECT aa.*, aav.attribute_value,  aav.attribute_value_int, aav.attribute_value_float, aav.assessment_id
+      ActiveRecord::Base.connection.exec_query(
+        'SELECT aa.*, aav.attribute_value,  aav.attribute_value_int, aav.attribute_value_float, aav.assessment_id
             FROM assessment_attribute_values aav
             JOIN assessment_attributes aa ON aav.attribute_id = aa.attribute_id',
-        )
-      end
+      )
+    end
 
-      it "the assessment_attribute_values table has data for each of the 3 test assessments" do
-        expect(
-          imported_data.group_by { |item| item["assessment_id"] }.count,
-        ).to eq(3)
-      end
+    it "the assessment_attribute_values table has data for each of the 3 test assessments" do
+      expect(
+        imported_data.group_by { |item| item["assessment_id"] }.count,
+      ).to eq(3)
+    end
 
-      it "the assessment_attribute_values has one entry for each type of assessment" do
-        select =
-          imported_data.select do |item|
-            item["attribute_name"] == "type_of_assessment"
-          end
-        expect(select.map { |item| item["attribute_value"] }).to eq(
-          %w[RdSAP CEPC SAP],
-        )
-      end
+    it "the assessment_attribute_values has one entry for each type of assessment" do
+      select =
+        imported_data.select do |item|
+          item["attribute_name"] == "type_of_assessment"
+        end
+      expect(select.map { |item| item["attribute_value"] }).to eq(
+        %w[RdSAP CEPC SAP],
+      )
+    end
 
-      it "the assessment_attribute_values table has CEPC data for asset_rating" do
-        arr =
-          imported_data.select do |result|
-            result["attribute_name"] == "asset_rating"
-          end
-        expect(arr[0]["attribute_value"]).to eq("80")
-        expect(arr[0]["parent_name"]).to eq(nil)
-        expect(arr[0]["attribute_value_int"]).to eq(80)
-      end
+    it "the assessment_attribute_values table has CEPC data for asset_rating" do
+      arr =
+        imported_data.select do |result|
+          result["attribute_name"] == "asset_rating"
+        end
+      expect(arr[0]["attribute_value"]).to eq("80")
+      expect(arr[0]["parent_name"]).to eq(nil)
+      expect(arr[0]["attribute_value_int"]).to eq(80)
+    end
 
-      it "returns SAP data for current_carbon_emission" do
-        arr =
-          imported_data.select do |result|
-            result["attribute_name"] == "current_carbon_emission"
-          end
-        expect(arr[0]["attribute_value"]).to eq("2.4")
-        expect(arr[0]["parent_name"]).to eq(nil)
-        expect(arr[0]["attribute_value_float"]).to eq(2.4)
-      end
+    it "returns SAP data for current_carbon_emission" do
+      arr =
+        imported_data.select do |result|
+          result["attribute_name"] == "current_carbon_emission"
+        end
+      expect(arr[0]["attribute_value"]).to eq("2.4")
+      expect(arr[0]["parent_name"]).to eq(nil)
+      expect(arr[0]["attribute_value_float"]).to eq(2.4)
+    end
 
-      it "the attribute table should contain one hash for the town with a parent of address" do
-        arr =
-          imported_data.select { |result| result["attribute_name"] == "town" }
-        expect(arr[0]["attribute_value"]).to eq("Whitbury")
-        expect(arr[0]["parent_name"]).to eq("address")
-      end
+    it "the attribute table should contain one hash for the town with a parent of address" do
+      arr =
+        imported_data.select { |result| result["attribute_name"] == "town" }
+      expect(arr[0]["attribute_value"]).to eq("Whitbury")
+      expect(arr[0]["parent_name"]).to eq("address")
     end
   end
 end
--- spec/unit/use_case/import_xml_certificate_spec.rb
+++ spec/unit/use_case/import_xml_certificate_spec.rb
@@ -1,37 +1,35 @@
 describe UseCase::ImportXmlCertificate do
-  context "call the use case import an xml document in the database" do
-    let(:gateway) do
-      Gateway::AssessmentAttributesGateway.new
-    end
+  let(:gateway) do
+    Gateway::AssessmentAttributesGateway.new
+  end
 
-    let!(:assessment_id) do
-      "0000-0000-0000-0000-0000"
-    end
+  let!(:assessment_id) do
+    "0000-0000-0000-0000-0000"
+  end
 
-    let!(:use_case) do
-      described_class.new(gateway)
-    end
+  let!(:use_case) do
+    described_class.new(gateway)
+  end
 
-    let!(:sample) do
-      Samples.xml("RdSAP-Schema-20.0.0")
-    end
+  let!(:sample) do
+    Samples.xml("RdSAP-Schema-20.0.0")
+  end
 
-    let!(:transformed_certificate) do
-      use_case.execute(sample, "RdSAP-Schema-20.0.0")
-    end
+  let!(:transformed_certificate) do
+    use_case.execute(sample, "RdSAP-Schema-20.0.0")
+  end
 
-    let(:saved_data) do
-      ActiveRecord::Base.connection.exec_query("SELECT * FROM assessment_attribute_values WHERE assessment_id = '#{assessment_id}'")
-    end
+  let(:saved_data) do
+    ActiveRecord::Base.connection.exec_query("SELECT * FROM assessment_attribute_values WHERE assessment_id = '#{assessment_id}'")
+  end
 
-    it "transforms the xml using the view model to_report method " do
-      expect(transformed_certificate).to be_a(Hash)
-      expect(transformed_certificate[:assessment_id]).to eq("4af9d2c31cf53e72ef6f59d3f59a1bfc500ebc2b1027bc5ca47361435d988e1a")
-    end
+  it "transforms the xml using the view model to_report method " do
+    expect(transformed_certificate).to be_a(Hash)
+    expect(transformed_certificate[:assessment_id]).to eq("4af9d2c31cf53e72ef6f59d3f59a1bfc500ebc2b1027bc5ca47361435d988e1a")
+  end
 
-    it "the attributes have been saved in the correct format" do
-      use_case.execute(sample, "RdSAP-Schema-20.0.0")
-      expect(saved_data.rows.length).not_to eq(0)
-    end
+  it "the attributes have been saved in the correct format" do
+    use_case.execute(sample, "RdSAP-Schema-20.0.0")
+    expect(saved_data.rows.length).not_to eq(0)
   end
 end
