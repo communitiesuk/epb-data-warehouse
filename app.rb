9b464d69b1a609390a6a836ce52d4f2566bbaa6f
Barry Halper <barryhalper@gmail.com>
2021-08-11 16:24:10 +0100

    removed app.rb from code base as no longer required

--- app.rb
+++
@@ -1,7 +0,0 @@
-require "pg" # postgresql
-require "erb"
-require "yaml"
-require "active_record"
-require "sinatra/activerecord"
-
-pp ActiveRecord::Base.connection.exec_query("SELECT * FROM assessment_look_ups")
