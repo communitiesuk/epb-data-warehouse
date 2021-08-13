4585320e2d5e325f5e75b336fe6499f85f335e56
Barry Halper <barryhalper@gmail.com>
2021-08-13 12:43:05 +0100

    EPBR-1419 Updated tests to account for number of times the upsert process happens

--- spec/unit/use_case/opt_out_certificate_spec.rb
+++ spec/unit/use_case/opt_out_certificate_spec.rb
@@ -20,22 +20,34 @@ describe UseCase::OptOutCertificate do
 
   context "when marking existing certs as opted out" do
     before do
-      allow(certificate_gateway).to receive(:fetch_meta_data).and_return({ optOut: true })
+      allow(certificate_gateway).to receive(:fetch_meta_data).with("1235-0000-0000-0000-0000").and_return({ optOut: true })
+      allow(certificate_gateway).to receive(:fetch_meta_data).with("0000-9999-0000-0000-0001").and_return({ optOut: true })
+      allow(certificate_gateway).to receive(:fetch_meta_data).with("0000-0000-0000-0000-0002").and_return({ optOut: true })
+      use_case.execute
     end
 
-    it "executes the update use case without error" do
-      expect { use_case.execute }.not_to raise_error
+    it "saves 3 opted out certificate " do
+      expect(database_gateway).to have_received(:add_attribute_value).exactly(3).times
     end
   end
 
-  context "when marking existing certs as opted in" do
+  context "when marking 1 existing certs as opted in" do
     before do
-      allow(certificate_gateway).to receive(:fetch_meta_data).and_return({ optOut: false })
+      allow(certificate_gateway).to receive(:fetch_meta_data).with("1235-0000-0000-0000-0000").and_return({ optOut: true })
+      allow(certificate_gateway).to receive(:fetch_meta_data).with("0000-9999-0000-0000-0001").and_return({ optOut: false })
+      allow(certificate_gateway).to receive(:fetch_meta_data).with("0000-0000-0000-0000-0002").and_return({ optOut: true })
       allow(database_gateway).to receive(:delete_attribute_value)
+      use_case.execute
+
     end
 
-    it "executes the update use case by deleting a record without error" do
-      expect { use_case.execute }.not_to raise_error
+    it "performs a save for each of the 3 certificates " do
+      expect(database_gateway).to have_received(:add_attribute_value).exactly(3).times
     end
+
+    it "executes the update use case by deleting one record " do
+      expect(database_gateway).to have_received(:delete_attribute_value).exactly(1).times
+    end
+
   end
 end
