e7957758bc6031fdd029723d78ebb4e0874f861c
Barry Halper <barryhalper@gmail.com>
2021-08-13 11:37:04 +0100

    EPBR-1473 Updated use case to : call the redis gateway to get the queue of rrns call the api gateway to get the meta data and extract the cancelled date call the db gateway to update the assessment with the cancelled_at attribute whose value is the date

--- lib/use_case/cancel_certificate.rb
+++ lib/use_case/cancel_certificate.rb
@@ -1,11 +1,19 @@
 module UseCase
   class CancelCertificate
-    def initialize(eav_gateway)
+    def initialize(eav_gateway:, redis_gateway:, api_gateway:)
       @assessment_attribute_gateway = eav_gateway
+      @redis_gateway = redis_gateway
+      @api_gateway = api_gateway
     end
 
-    def execute(assessment_id)
-      @assessment_attribute_gateway.update_assessment_attribute(assessment_id, "")
+    def execute
+      assessment_ids = @redis_gateway.consume_queue "cancelled"
+      assessment_ids.each do |assessment_id|
+        meta_data = @api_gateway.fetch_meta_data(assessment_id)
+        next if meta_data[:cancelledAt].nil?
+
+        @assessment_attribute_gateway.add_attribute_value(assessment_id: assessment_id, attribute_name: "cancelled_at", attribute_value: meta_data[:cancelledAt])
+      end
     end
   end
 end
