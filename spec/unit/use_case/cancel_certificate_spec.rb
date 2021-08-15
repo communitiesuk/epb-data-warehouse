43824189920f4731689cf06c0c871d73cf44cf5d
Tony <tony@madtech.com>
2021-08-15 23:04:59 +0100

    EPBR-1463 Remove superfluous test in ImportXMLCert use-case specs

--- spec/unit/use_case/cancel_certificate_spec.rb
+++ spec/unit/use_case/cancel_certificate_spec.rb
@@ -1,13 +1,13 @@
 describe UseCase::CancelCertificate do
   subject(:use_case) do
     described_class.new(
-      eav_gateway: database_gateway,
+      eav_gateway: eav_database_gateway,
       redis_gateway: redis_gateway,
       api_gateway: api_gateway,
     )
   end
 
-  let(:database_gateway) do
+  let(:eav_database_gateway) do
     instance_double(Gateway::AssessmentAttributesGateway)
   end
 
@@ -20,22 +20,22 @@ describe UseCase::CancelCertificate do
   end
 
   before do
-    allow(database_gateway).to receive(:add_attribute_value).and_return(true)
+    allow(eav_database_gateway).to receive(:add_attribute_value).and_return(true)
     allow(redis_gateway).to receive(:consume_queue).and_return(%w[1235-0000-0000-0000-0000 0000-9999-0000-0000-0001 0000-0000-0000-0000-0002])
   end
 
-  context "when processing cancellations where there is a cancelled at attribute present" do
+  context "when processing cancellations where there is a cancelled_at attribute present" do
     before do
       allow(api_gateway).to receive(:fetch_meta_data).and_return({ cancelledAt: "2021-08-13T08:12:51.205Z" })
     end
 
-    it "saves the relevants certificates to database" do
+    it "saves the relevant certificates to database" do
       expect { use_case.execute }.not_to raise_error
-      expect(database_gateway).to have_received(:add_attribute_value).exactly(3).times
+      expect(eav_database_gateway).to have_received(:add_attribute_value).exactly(3).times
     end
   end
 
-  context "when processing cancellations where there is  a certificate without a cancelled at date" do
+  context "when processing cancellations where there is a certificate without a cancelled_at date" do
     before do
       allow(api_gateway).to receive(:fetch_meta_data).with("1235-0000-0000-0000-0000").and_return({ cancelledAt: Time.now })
       allow(api_gateway).to receive(:fetch_meta_data).with("0000-9999-0000-0000-0001").and_return({ cancelledAt: nil })
@@ -44,7 +44,7 @@ describe UseCase::CancelCertificate do
     end
 
     it "skips over the certificate whose cancellation date is null" do
-      expect(database_gateway).to have_received(:add_attribute_value).exactly(2).times
+      expect(eav_database_gateway).to have_received(:add_attribute_value).exactly(2).times
     end
   end
 end
--- spec/unit/use_case/import_xml_certificate_spec.rb
+++ spec/unit/use_case/import_xml_certificate_spec.rb
@@ -7,26 +7,22 @@ describe UseCase::ImportXmlCertificate do
     instance_double(Gateway::RegisterApiGateway)
   end
 
-  let!(:assessment_id) do
+  let(:assessment_id) do
     "0000-0000-0000-0000-0000"
   end
 
-  let!(:use_case) do
+  let(:use_case) do
     described_class.new(database_gateway, certificate_gateway)
   end
 
-  let!(:sample) do
+  let(:sample) do
     Samples.xml("RdSAP-Schema-20.0.0")
   end
 
-  let!(:transformed_certificate) do
+  let(:transformed_certificate) do
     allow(certificate_gateway).to receive(:fetch).and_return(sample)
     allow(certificate_gateway).to receive(:fetch_meta_data).and_return({ schemaType: "RdSAP-Schema-20.0.0", assessmentAddressId: "UPRN-000000000000" })
     use_case.execute(assessment_id)
-  end
-
-  let(:saved_data) do
-    ActiveRecord::Base.connection.exec_query("SELECT * FROM assessment_attribute_values WHERE assessment_id = '#{assessment_id}'")
   end
 
   it "transforms the xml using the view model to_report method " do
@@ -34,10 +30,4 @@ describe UseCase::ImportXmlCertificate do
     expect(transformed_certificate[:assessment_id]).to eq("4af9d2c31cf53e72ef6f59d3f59a1bfc500ebc2b1027bc5ca47361435d988e1a")
   end
 
-  it "the attributes have been saved in the correct format" do
-    # pp sample
-    pp saved_data
-    # use_case.execute(sample)
-    expect(saved_data.rows.length).not_to eq(0)
-  end
 end
