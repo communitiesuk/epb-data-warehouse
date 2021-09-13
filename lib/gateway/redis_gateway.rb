d9c5f8b5bf945811777fdd146c238f58b15ccbb8
Aga Dufrat <a.dufrat@gmail.com>
2021-09-13 14:54:55 +0100

    Use symbols for Redis queue names
    also renames "opt-out" queue to "opt_outs" so it's consistent with "assessments"

--- lib/gateway/redis_gateway.rb
+++ lib/gateway/redis_gateway.rb
@@ -9,13 +9,13 @@ module Gateway
     def consume_queue(queue_name, count: 50)
       futures = []
       redis.pipelined do
-        count.to_i.times { futures << redis.rpop(queue_name) }
+        count.to_i.times { futures << redis.rpop(queue_name.to_s) }
       end
       futures.map(&:value).compact
     end
 
     def push_to_queue(queue_name, data)
-      redis.lpush(queue_name, data)
+      redis.lpush(queue_name.to_s, data)
     end
 
   private
--- spec/unit/gateway/redis_gateway_spec.rb
+++ spec/unit/gateway/redis_gateway_spec.rb
@@ -17,28 +17,29 @@ describe Gateway::RedisGateway do
 
   context "when processing a queue of IDs" do
     it "can consume the IDs back out of the queue when pushed on in one push" do
-      gateway.push_to_queue "assessments", ids
-      expect(gateway.consume_queue("assessments")).to eq ids
+      gateway.push_to_queue(:assessments, ids)
+      expect(gateway.consume_queue(:assessments)).to eq ids
     end
 
     it "can consume the IDs back out of the queue when pushed onto the queue one by one" do
-      ids.each { |id| gateway.push_to_queue "assessments", id }
-      expect(gateway.consume_queue("assessments")).to eq ids
+      ids.each { |id| gateway.push_to_queue(:assessments, id) }
+      expect(gateway.consume_queue(:assessments)).to eq ids
     end
 
     it "leaves an empty queue once a relatively small queue has been consumed" do
-      gateway.push_to_queue "assessments", ids
-      gateway.consume_queue("assessments")
-      expect(gateway.consume_queue("assessments")).to eq []
+      gateway.push_to_queue(:assessments, ids)
+      gateway.consume_queue(:assessments)
+
+      expect(gateway.consume_queue(:assessments)).to eq []
     end
 
     context "when queue is populated with more IDs than the default consume count of 50" do
       before do
-        gateway.push_to_queue("assessments", (1..75).collect { |_| SecureRandom.uuid })
+        gateway.push_to_queue(:assessments, (1..75).collect { |_| SecureRandom.uuid })
       end
 
       it "consumes the default consume count of 50, leaving the expected remainder on the queue" do
-        consumed = gateway.consume_queue("assessments").length
+        consumed = gateway.consume_queue(:assessments).length
         remainder = redis.llen("assessments").to_i
         expect([consumed, remainder]).to eq [50, 25]
       end
