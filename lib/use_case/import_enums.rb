1042eacbd11d9cb67bd292f01602cc901b28a6aa
Aga Dufrat <a.dufrat@gmail.com>
2021-10-07 10:54:15 +0100

    Refactor UseCase::ImportEnums

--- lib/use_case/import_enums.rb
+++ lib/use_case/import_enums.rb
@@ -8,32 +8,36 @@ module UseCase
 
     def execute(attribute_mappings)
       attribute_mappings.each do |attribute|
-        attribute_id = @assessment_attribute_gateway.get_attribute_id(attribute["attribute_name"])
         enum_hashes = @xsd_presenter.get_enums_by_type(attribute["xsd_node_name"])
+
         if @xsd_presenter.variation_between_schema_versions?(enum_hashes)
           enum_hashes.each do |schema_version, values|
-            values.each do |key, value|
-              @assessment_lookups_gateway.add_lookup(Domain::AssessmentLookup.new(
-                                                       lookup_key: key,
-                                                       lookup_value: value,
-                                                       attribute_id: attribute_id,
-                                                       type_of_assessment: attribute["type_of_assessment"],
-                                                       schema_version: schema_version,
-                                                     ))
-            end
+            save_lookup(values: values, attribute: attribute, schema_version: schema_version)
           end
         else
-          enum_hashes.first[1].each do |key, value|
-            @assessment_lookups_gateway.add_lookup(Domain::AssessmentLookup.new(
-                                                     lookup_key: key,
-                                                     lookup_value: value,
-                                                     attribute_id: attribute_id,
-                                                     type_of_assessment: nil,
-                                                     schema_version: nil,
-                                                   ))
-          end
+          save_lookup(values: enum_hashes.first[1], attribute: attribute)
         end
       end
+    end
+
+    private
+
+    def save_lookup(values:, attribute:, schema_version: nil)
+      attribute_id = attribute_id(attribute)
+
+      values.each do |key, value|
+        @assessment_lookups_gateway.add_lookup(Domain::AssessmentLookup.new(
+                                                 lookup_key: key,
+                                                 lookup_value: value,
+                                                 attribute_id: attribute_id,
+                                                 type_of_assessment: attribute["type_of_assessment"] || nil,
+                                                 schema_version: schema_version,
+                                               ))
+      end
+    end
+
+    def attribute_id(attribute)
+      @assessment_attribute_gateway.get_attribute_id(attribute["attribute_name"])
     end
   end
 end
