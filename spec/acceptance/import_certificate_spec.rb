0a6b0ea4dd8464deb7fe8039cef6b9b8c2d9c74a
Christine Horrocks <splogy2001@hotmail.com>
2021-11-09 12:02:24 +0000

    Add end to end acceptance test for RdSAP

--- spec/acceptance/import_certificate_spec.rb
+++ spec/acceptance/import_certificate_spec.rb
@@ -3,16 +3,10 @@ describe "Acceptance::ImportCertificate" do
     UseCase::ImportCertificates.new(Gateway::AssessmentAttributesGateway.new, certificate_gateway, redis_gateway)
   end
 
-  # let(:attribute_values) do
-  #   ActiveRecord::Base.connection.exec_query(
-  #     "SELECT * FROM assessment_attribute_values WHERE attribute_value = 'RdSAP-Schema-20.0.0'",
-  #   )
-  # end
-
-  def attribute_values(attribute)
-      ActiveRecord::Base.connection.exec_query(
-        "SELECT * FROM assessment_attribute_values",
-      )
+  def attributes_values_from_database(column, attribute)
+    ActiveRecord::Base.connection.exec_query(
+      "SELECT * FROM assessment_attribute_values WHERE #{column} = #{attribute}",
+    )
   end
 
   let(:certificate_gateway) do
@@ -53,21 +47,29 @@ describe "Acceptance::ImportCertificate" do
     allow(Gateway::RegisterApiGateway).to receive(:new).and_return(certificate_gateway)
     allow(certificate_gateway).to receive(:fetch).and_return(xml_sample)
     allow(certificate_gateway).to receive(:fetch_meta_data).and_return(meta_data_sample)
+    redis_gateway.push_to_queue(:assessments, ids)
+    usecase.execute
   end
 
-  context "When an assessment id is provided to the queue" do
+  context "when an assessment id is provided to the queue" do
 
-    before do
-      redis_gateway.push_to_queue(:assessments, ids)
+    it "saves the relevant attributes values to the database" do
+      response = attributes_values_from_database("attribute_value", "'RdSAP-Schema-20.0.0'")
+      expect(response.length).to be >= 1
     end
 
-    it "saves the relevant attributes to the database" do
-      subject.execute
-      json_attributes = attribute_values("'RdSAP-Schema-20.0.0'")
-      # pp json_attributes
-      # ruby_attributes = JSON.parse(json_attributes)
-      # pp ruby_attributes
-      # expect(ruby_attributes.first["attribute_value"]).to eq("RdSAP-Schema-20.0.0")
+    it "saves the relevant json to the database" do
+      response = attributes_values_from_database("json", "'{\"u_value\": 2, \"data_source\": 2, \"solar_transmittance\": 0.72}'")
+      expect(response.length).to be >= 1
+    end
+  end
+
+  context "when json is entered into the attribute value table" do
+    it "is queryable" do
+      response = ActiveRecord::Base.connection.exec_query(
+        "select * from assessment_attribute_values WHERE json->>'stone_walls' like 'true'",
+      )
+      expect(response.length).to be >= 1
     end
   end
 end
