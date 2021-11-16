95aba42f907fc4e34209ea234553ba2c3e3e9482
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2021-11-16 13:53:25 +0000

    add ability to define an xml parser configuration that can pick out the correct root node at runtime

--- lib/xml_presenter/to_warehouse/base_configuration.rb
+++ lib/xml_presenter/to_warehouse/base_configuration.rb
@@ -1,8 +1,10 @@
 module XmlPresenter
   module ToWarehouse
     class BaseConfiguration
-      def to_args
-        self.class.args
+      def to_args(sub_node_value: nil)
+        self.class.args.merge(
+          { specified_report: self.class.root_node_option(sub_node_value: sub_node_value) },
+        ).compact
       end
 
       class << self
@@ -11,6 +13,19 @@ module XmlPresenter
         KEYS.each do |key|
           define_method key do |arg|
             args[key] = arg
+          end
+        end
+
+        def pick_root_node(root_node:, sub_node:)
+          @root_node_partial_option = {
+            root_node: root_node,
+            sub_node: sub_node,
+          }
+        end
+
+        def root_node_option(sub_node_value:)
+          if @root_node_partial_option && sub_node_value
+            @root_node_partial_option.merge({ sub_node_value: sub_node_value })
           end
         end
 
--- spec/xml_presenter/to_warehouse/base_configuration_spec.rb
+++ spec/xml_presenter/to_warehouse/base_configuration_spec.rb
@@ -5,18 +5,38 @@ class ExampleConfig < XmlPresenter::ToWarehouse::BaseConfiguration
   preferred_keys({ "Outdated-Term" => "newfangled_term" })
   list_nodes %w[List-1 List-2]
   rootless_list_nodes({ "A-Random-List-Item" => "random_list" })
+  pick_root_node root_node: "My-Root", sub_node: "Id-Node"
 end
 
 RSpec.describe XmlPresenter::ToWarehouse::BaseConfiguration do
-  specify "#to_args" do
-    expected_args = {
-      excludes: %w[Exclude-1 Exclude-2],
-      includes: %w[Include-1 Include-2],
-      bases: %w[Base-1 Base-2 Base-3],
-      preferred_keys: { "Outdated-Term" => "newfangled_term" },
-      list_nodes: %w[List-1 List-2],
-      rootless_list_nodes: { "A-Random-List-Item" => "random_list" },
-    }
-    expect(ExampleConfig.new.to_args).to eq expected_args
+  context "when no sub node index is passed" do
+    specify "#to_args" do
+      expected_args = {
+        excludes: %w[Exclude-1 Exclude-2],
+        includes: %w[Include-1 Include-2],
+        bases: %w[Base-1 Base-2 Base-3],
+        preferred_keys: { "Outdated-Term" => "newfangled_term" },
+        list_nodes: %w[List-1 List-2],
+        rootless_list_nodes: { "A-Random-List-Item" => "random_list" },
+      }
+      expect(ExampleConfig.new.to_args).to eq expected_args
+    end
+  end
+
+  context "when a sub node index is passed" do
+    let(:sub_node_value) { "the sub node value we are looking for" }
+
+    specify "#to_args" do
+      expected_args = {
+        excludes: %w[Exclude-1 Exclude-2],
+        includes: %w[Include-1 Include-2],
+        bases: %w[Base-1 Base-2 Base-3],
+        preferred_keys: { "Outdated-Term" => "newfangled_term" },
+        list_nodes: %w[List-1 List-2],
+        rootless_list_nodes: { "A-Random-List-Item" => "random_list" },
+        specified_report: { root_node: "My-Root", sub_node: "Id-Node", sub_node_value: sub_node_value },
+      }
+      expect(ExampleConfig.new.to_args(sub_node_value: sub_node_value)).to eq expected_args
+    end
   end
 end
