dc04fa53594666a318dc0d441bde547f49f792d3
Barry Halper <barryhalper@gmail.com>
2021-12-01 16:04:32 +0000

    Updated the add_assessment method of the doc gateway to use UPSERT methodology

--- lib/gateway/documents_gateway.rb
+++ lib/gateway/documents_gateway.rb
@@ -1,60 +1,35 @@
 module Gateway
   class DocumentsGateway
     def add_assessment(assessment_id:, document:)
-      case record_exists?(assessment_id: assessment_id)
-      when false
-        sql = <<-SQL
+      sql = <<-SQL
         INSERT INTO assessment_documents (assessment_id, document, created_at, updated_at) VALUES ($1, $2, $3, $4)
-        SQL
+           ON CONFLICT(assessment_id)
+        DO UPDATE SET document=$2, updated_at=$3
+      SQL
 
-        bindings = [
-          ActiveRecord::Relation::QueryAttribute.new(
-            "assessment_id",
-            assessment_id,
-            ActiveRecord::Type::String.new,
-          ),
-          ActiveRecord::Relation::QueryAttribute.new(
-            "document",
-            document,
-            ActiveRecord::Type::Json.new,
-          ),
-          ActiveRecord::Relation::QueryAttribute.new(
-            "created_at",
-            Time.now.utc,
-            ActiveRecord::Type::DateTime.new,
-          ),
-          ActiveRecord::Relation::QueryAttribute.new(
-            "updated_at",
-            Time.now.utc,
-            ActiveRecord::Type::DateTime.new,
-          ),
-        ]
-        ActiveRecord::Base.connection.exec_query(sql, "SQL", bindings)
-      when true
-        update_sql = <<-SQL
-          UPDATE assessment_documents SET document=$1, updated_at=$2 WHERE assessment_id=$3
-        SQL
-
-        update_bindings = [
-          ActiveRecord::Relation::QueryAttribute.new(
-            "document",
-            document,
-            ActiveRecord::Type::Json.new,
-          ),
-          ActiveRecord::Relation::QueryAttribute.new(
-            "updated_at",
-            Time.now.utc,
-            ActiveRecord::Type::DateTime.new,
-          ),
-          ActiveRecord::Relation::QueryAttribute.new(
-            "assessment_id",
-            assessment_id,
-            ActiveRecord::Type::String.new,
-          ),
-        ]
-
-        ActiveRecord::Base.connection.exec_query(update_sql, "UPDATE_SQL", update_bindings)
-      end
+      bindings = [
+        ActiveRecord::Relation::QueryAttribute.new(
+          "assessment_id",
+          assessment_id,
+          ActiveRecord::Type::String.new,
+        ),
+        ActiveRecord::Relation::QueryAttribute.new(
+          "document",
+          document,
+          ActiveRecord::Type::Json.new,
+        ),
+        ActiveRecord::Relation::QueryAttribute.new(
+          "created_at",
+          Time.now.utc,
+          ActiveRecord::Type::DateTime.new,
+        ),
+        ActiveRecord::Relation::QueryAttribute.new(
+          "updated_at",
+          Time.now.utc,
+          ActiveRecord::Type::DateTime.new,
+        ),
+      ]
+      ActiveRecord::Base.connection.exec_query(sql, "SQL", bindings)
     end
 
     def set_top_level_attribute(assessment_id:, top_level_attribute:, new_value:)
@@ -102,24 +77,6 @@ module Gateway
       ]
 
       ActiveRecord::Base.connection.exec_query(sql, "SET_ATTR_SQL", bindings)
-    end
-
-  private
-
-    def record_exists?(assessment_id:)
-      count_sql = <<-SQL
-        SELECT COUNT(*) AS cnt FROM assessment_documents WHERE assessment_id=$1
-      SQL
-
-      bindings = [
-        ActiveRecord::Relation::QueryAttribute.new(
-          "assessment_id",
-          assessment_id,
-          ActiveRecord::Type::String.new,
-        ),
-      ]
-
-      ActiveRecord::Base.connection.exec_query(count_sql, "COUNT_SQL", bindings).first["cnt"] == 1
     end
   end
 end
