fe642222b5cc724a56386d8d73909429d9559c34
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2022-01-14 17:47:51 +0000

    add ONS UPRN Directory tables with script to stream zip resources directly into db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_101811) do
+ActiveRecord::Schema.define(version: 2022_01_28_122223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,21 @@ ActiveRecord::Schema.define(version: 2021_11_18_101811) do
     t.index ["lookup_value"], name: "index_assessment_lookups_on_lookup_value"
   end
 
+  create_table "ons_uprn_directory", id: false, force: :cascade do |t|
+    t.string "uprn", limit: 17, null: false
+    t.string "postcode", limit: 8, null: false
+    t.jsonb "areas", null: false
+    t.integer "version_id", null: false
+    t.index ["uprn"], name: "index_ons_uprn_directory_on_uprn"
+  end
+
+  create_table "ons_uprn_directory_versions", id: :serial, force: :cascade do |t|
+    t.string "version_month", limit: 7, null: false
+    t.index ["version_month"], name: "index_ons_uprn_directory_versions_on_version_month", unique: true
+  end
+
   add_foreign_key "assessment_attribute_lookups", "assessment_attributes", column: "attribute_id", primary_key: "attribute_id"
   add_foreign_key "assessment_attribute_lookups", "assessment_lookups", column: "lookup_id"
   add_foreign_key "assessment_attribute_values", "assessment_attributes", column: "attribute_id", primary_key: "attribute_id"
+  add_foreign_key "ons_uprn_directory", "ons_uprn_directory_versions", column: "version_id"
 end
