53e2704b8325201cdda0878eca853c243d28b8be
Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>
2023-01-13 14:53:38 +0000

    add migration to convert existing main-heating nodes to main_heating

---
+++ db/migrate/20230113143331_correct_main_heating_node.rb
@@ -0,0 +1,9 @@
+class CorrectMainHeatingNode < ActiveRecord::Migration[7.0]
+  def self.up
+    execute "UPDATE assessment_documents SET document = document - 'main-heating' || jsonb_build_object('main_heating', document -> 'main-heating'), updated_at = NOW() WHERE document ? 'main-heating'"
+    execute "UPDATE assessment_attribute_values SET attribute_id=(SELECT attribute_id FROM assessment_attributes WHERE attribute_name = 'main_heating') WHERE attribute_id=(SELECT attribute_id FROM assessment_attributes WHERE attribute_name = 'main-heating')"
+  end
+  def self.down
+    # no down migration necessary as this is a data fix
+  end
+end
