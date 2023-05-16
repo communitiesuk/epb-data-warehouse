6d6a5fdabd1fb9a6f533035a3c929bef5a39a8c1
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2023-05-16 16:11:30 +0100

    EPBR-3251 feat: moved profile in run migrate script to the end of line

--- buildspec/run_db_migrate_task.sh
+++ buildspec/run_db_migrate_task.sh
@@ -10,9 +10,7 @@ CLUSTER_NAME="${PREFIX}-warehouse-cluster"
 TASK="${PREFIX}-warehouse-ecs-db-migrate-task"
 
 
-aws ec2 describe-vpcs --profile $PROFILE
-
-VPC_ID=$(aws ec2 describe-vpcs --profile $PROFILE --filters Name=tag:Name,Values=$VPC_NAME --query 'Vpcs[0].VpcId')
+VPC_ID=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=$VPC_NAME --query 'Vpcs[0].VpcId' --profile $PROFILE)
 printf "VPC ID=" $VPC_ID
 
 if [[ $VPC_ID = "" ]]; then
@@ -20,30 +18,30 @@ if [[ $VPC_ID = "" ]]; then
   exit 1
 fi
 
-SUBNET_GROUP_ID=$(aws ec2 describe-subnets --profile $PROFILE --filter Name=vpc-id,Values=$VPC_ID --query 'Subnets[?MapPublicIpOnLaunch==`false`].SubnetId')
+SUBNET_GROUP_ID=$(aws ec2 describe-subnets --filter Name=vpc-id,Values=$VPC_ID --query 'Subnets[?MapPublicIpOnLaunch==`false`].SubnetId' --profile $PROFILE)
 
 printf "SUBNET_GROUP ID=" $SUBNET_GROUP_ID
 
-SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --profile $PROFILE  --filter Name=group-name,Values=$SECURITY_GROUP_NAME --query 'SecurityGroups[0].GroupId')
+SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=$SECURITY_GROUP_NAME --query 'SecurityGroups[0].GroupId' --profile $PROFILE )
 
 printf "SECURITY_GROUP ID=" $SECURITY_GROUP_ID
 
 JSON_STRING="{\"awsvpcConfiguration\": {\"subnets\": ${SUBNET_GROUP_ID}, \"securityGroups\": [${SECURITY_GROUP_ID}],\"assignPublicIp\":\"DISABLED\"}}"
 
-TASK_ID=$(aws ecs run-task  --profile $PROFILE  --cluster $CLUSTER_NAME  --task-definition epb-intg-warehouse-ecs-db-migrate-task  \
+TASK_ID=$(aws ecs run-task  --cluster $CLUSTER_NAME  --task-definition epb-intg-warehouse-ecs-db-migrate-task  \
     --network-configuration "${JSON_STRING}" \
-    --launch-type "FARGATE" --query 'tasks[0].containers[0].taskArn' | tr -d '"' )
+    --launch-type "FARGATE" --query 'tasks[0].containers[0].taskArn' --profile $PROFILE | tr -d '"' )
 
 STATUS=""
 
 while [[ $STATUS != "\"STOPPED\"" ]]; do
-STATUS=$(aws ecs describe-tasks --profile $PROFILE  --cluster $CLUSTER_NAME --tasks $TASK_ID --query 'tasks[0].containers[0].lastStatus')
+STATUS=$(aws ecs describe-tasks  --cluster $CLUSTER_NAME --tasks $TASK_ID --query 'tasks[0].containers[0].lastStatus' --profile $PROFILE)
 printf "${STATUS} << WAITING FOR MIGRATION TASK TO COMPLETE...\n"
 
 sleep 5
 done
 
-EXIT_CODE=$(aws ecs describe-tasks --profile $PROFILE --cluster $CLUSTER_NAME --tasks $TASK_ID --query 'tasks[0].containers[0].exitCode')
+EXIT_CODE=$(aws ecs describe-tasks --cluster $CLUSTER_NAME --tasks $TASK_ID --query 'tasks[0].containers[0].exitCode' --profile $PROFILE)
 if [[ $EXIT_CODE = 0 ]]; then
   printf "${TASK_ID} << MIGRATION TASK COMPLETED"
   exit 0
