85190e926f0b396ab58f8f687afc917778cd758e
Alice Lui <54395483+al01010@users.noreply.github.com>
2023-09-13 09:45:28 +0100

    Update trim postcode migration with WHERE clause for the ons_uprn_directory

--- db/migrate/20230912142153_trim_ons_postcodes.rb
+++ db/migrate/20230912142153_trim_ons_postcodes.rb
@@ -1,6 +1,5 @@
 class TrimOnsPostcodes < ActiveRecord::Migration[7.0]
   def self.up
-    execute "UPDATE ons_uprn_directory SET postcode = TRIM(postcode)"
+    execute "UPDATE ons_uprn_directory SET postcode = TRIM(postcode) WHERE postcode != TRIM(postcode)"
   end
-
 end
