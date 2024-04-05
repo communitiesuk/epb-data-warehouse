ffc9f1a2a24834b6fc27fa1f0cbe434683414a62
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2024-04-05 16:15:44 +0100

    EPBR-5561 fix: insert ons csv data by row rather than using COPY

--- spec/shared_context/shared_ons_data.rb
+++ spec/shared_context/shared_ons_data.rb
@@ -1,20 +1,23 @@
 shared_context "when saving ons data" do
   def import_postcode_directory_data
     file_path = File.join Dir.pwd, "spec/fixtures/ons_data/ons_postcode_directory.csv"
-    sql = "TRUNCATE TABLE  ons_postcode_directory;
-            COPY ons_postcode_directory(postcode,country_code,region_code,local_authority_code,westminster_parliamentary_constituency_code,other_areas)
-            FROM '#{file_path}'
-            DELIMITER ','
-            CSV HEADER;"
-    ActiveRecord::Base.connection.execute(sql)
+    ActiveRecord::Base.connection.execute("TRUNCATE ons_postcode_directory")
+    CSV.foreach(file_path, headers: true) do |row|
+      sql = "INSERT INTO ons_postcode_directory(postcode,country_code,region_code,local_authority_code,westminster_parliamentary_constituency_code,other_areas)
+            VALUES ('#{row["postcode"]}','#{row["country_code"]}','#{row["region_code"]}', '#{row["local_authority_code"]}', '#{row["westminster_parliamentary_constituency_code"]}','#{row["other_areas"]}' )"
+      ActiveRecord::Base.connection.execute(sql)
+    end
+
   end
 
   def import_postcode_directory_name
     file_path = File.join Dir.pwd, "spec/fixtures/ons_data/ons_postcode_directory_names.csv"
-    sql = "TRUNCATE TABLE  ons_postcode_directory_names; COPY ons_postcode_directory_names(area_code,name,type,type_code)
-            FROM '#{file_path}'
-            DELIMITER ','
-            CSV HEADER;"
-    ActiveRecord::Base.connection.execute(sql)
+    ActiveRecord::Base.connection.execute("TRUNCATE ons_postcode_directory_names")
+    CSV.foreach(file_path, headers: true) do |row|
+      sql = "INSERT INTO ons_postcode_directory_names(area_code,name,type,type_code)
+        VALUES ('#{row["area_code"]}', '#{row["name"]}', '#{row["type"]}', '#{row["type_code"]}')"
+      ActiveRecord::Base.connection.execute(sql)
+    end
+
   end
 end
