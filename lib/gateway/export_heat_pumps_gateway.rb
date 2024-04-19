6293bb21008b97fb2d7d145638243d4be811d0d7
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2024-04-19 18:52:30 +0100

    Tidy up some SQL formatting

--- lib/gateway/export_heat_pumps_gateway.rb
+++ lib/gateway/export_heat_pumps_gateway.rb
@@ -9,21 +9,21 @@ module Gateway
     def fetch_by_property_type(start_date:, end_date:)
       sql = <<-SQL
        SELECT
-           al.lookup_value as property_type,
-           count(assessment_id) as number_of_assessments
-        FROM assessment_documents ad
-        JOIN assessment_lookups al ON al.lookup_key = ad.document ->> 'property_type'
-        JOIN (
-            select distinct lookup_id from assessment_attribute_lookups aal
-            join assessment_attributes aa on aal.attribute_id = aa.attribute_id
-            where aa.attribute_name = 'property_type' and type_of_assessment = 'SAP'
-        ) as aal on aal.lookup_id = al.id
-        WHERE  ad.document->>'registration_date' BETWEEN $1 AND $2
-        AND ad.document ->> 'assessment_type' = $3
-        AND ad.document ->> 'transaction_type' = $4
-        AND ad.document ->> 'postcode' NOT LIKE $5
-        AND ((ad.document -> 'main_heating')::varchar ILIKE $6 or (ad.document -> 'main_heating')::varchar ILIKE $7)
-        GROUP BY al.lookup_value;
+           al.lookup_value AS property_type,
+           COUNT(assessment_id) AS number_of_assessments
+       FROM assessment_documents ad
+         JOIN assessment_lookups al ON al.lookup_key = ad.document ->> 'property_type'
+         JOIN (
+            SELECT DISTINCT lookup_id FROM assessment_attribute_lookups aal
+            JOIN assessment_attributes aa ON aal.attribute_id = aa.attribute_id
+            WHERE aa.attribute_name = 'property_type' AND type_of_assessment = 'SAP'
+         ) AS aal ON aal.lookup_id = al.id
+       WHERE  ad.document->>'registration_date' BETWEEN $1 AND $2
+         AND ad.document ->> 'assessment_type' = $3
+         AND ad.document ->> 'transaction_type' = $4
+         AND ad.document ->> 'postcode' NOT LIKE $5
+         AND ((ad.document -> 'main_heating')::varchar ILIKE $6 OR (ad.document -> 'main_heating')::varchar ILIKE $7)
+       GROUP BY al.lookup_value;
       SQL
 
       ActiveRecord::Base.connection.exec_query(sql, "SQL", bindings(start_date:, end_date:)).map { |result| result }
@@ -32,30 +32,30 @@ module Gateway
     def fetch_by_floor_area(start_date:, end_date:)
       sql = <<-SQL
        SELECT
-           CASE
-               WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 0 AND 50 THEN 'BETWEEN 0 AND 50'
+         CASE
+           WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 0 AND 50 THEN 'BETWEEN 0 AND 50'
            WHEN (ad.document ->> 'total_floor_area')::numeric  BETWEEN 51 AND 100 THEN 'BETWEEN 51 AND 100'
            WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 101 AND 150 THEN 'BETWEEN 101 AND 150'
            WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 151 AND 200 THEN 'BETWEEN 151 AND 200'
-          WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 201 AND 250 THEN 'BETWEEN 201 AND 250'
+           WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 201 AND 250 THEN 'BETWEEN 201 AND 250'
            WHEN (ad.document ->> 'total_floor_area')::numeric >= 251 THEN 'GREATER THAN 251'
-           END as total_floor_area,
-            count(assessment_id) as number_of_assessments
+         END AS total_floor_area,
+             COUNT(assessment_id) AS number_of_assessments
        FROM assessment_documents ad
-         WHERE  ad.document->>'registration_date' BETWEEN $1 AND $2
-        AND ad.document ->> 'assessment_type' = $3
-        AND ad.document ->> 'transaction_type' = $4
-        AND ad.document ->> 'postcode' NOT LIKE $5
-        AND ((ad.document -> 'main_heating')::varchar ILIKE $6 or (ad.document -> 'main_heating')::varchar ILIKE $7)
-        GROUP BY
-          CASE
-                 WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 0 AND 50 THEN 'BETWEEN 0 AND 50'
+       WHERE  ad.document->>'registration_date' BETWEEN $1 AND $2
+         AND ad.document ->> 'assessment_type' = $3
+         AND ad.document ->> 'transaction_type' = $4
+         AND ad.document ->> 'postcode' NOT LIKE $5
+         AND ((ad.document -> 'main_heating')::varchar ILIKE $6 OR (ad.document -> 'main_heating')::varchar ILIKE $7)
+       GROUP BY
+         CASE
+           WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 0 AND 50 THEN 'BETWEEN 0 AND 50'
            WHEN (ad.document ->> 'total_floor_area')::numeric  BETWEEN 51 AND 100 THEN 'BETWEEN 51 AND 100'
            WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 101 AND 150 THEN 'BETWEEN 101 AND 150'
            WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 151 AND 200 THEN 'BETWEEN 151 AND 200'
-          WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 201 AND 250 THEN 'BETWEEN 201 AND 250'
+           WHEN (ad.document ->> 'total_floor_area')::numeric BETWEEN 201 AND 250 THEN 'BETWEEN 201 AND 250'
            WHEN (ad.document ->> 'total_floor_area')::numeric >= 251 THEN 'GREATER THAN 251'
-           END
+         END
       SQL
 
       ActiveRecord::Base.connection.exec_query(sql, "SQL", bindings(start_date:, end_date:)).map { |result| result }
@@ -63,16 +63,15 @@ module Gateway
 
     def fetch_by_local_authority(start_date:, end_date:)
       sql = <<~SQL
-              SELECT COUNT(DISTINCT assessment_id) as number_of_assessments,
-              onsn.name as local_authority
-                FROM assessment_documents ad
-                left join ons_postcode_directory ons on ad.document ->> 'postcode' = ons.postcode
-                left join ons_postcode_directory_names onsn on ons.local_authority_code = onsn.area_code
-         WHERE  ad.document->>'registration_date' BETWEEN $1 AND $2
-        AND ad.document ->> 'assessment_type' = $3
-        AND ad.document ->> 'transaction_type' = $4
-        AND ad.document ->> 'postcode' NOT LIKE $5
-        AND ((ad.document -> 'main_heating')::varchar  ILIKE $6 or (ad.document -> 'main_heating')::varchar ILIKE $7)
+        SELECT COUNT(DISTINCT assessment_id) AS number_of_assessments, onsn.name AS local_authority
+        FROM assessment_documents ad
+          LEFT JOIN ons_postcode_directory ons ON ad.document ->> 'postcode' = ons.postcode
+          LEFT JOIN ons_postcode_directory_names onsn ON ons.local_authority_code = onsn.area_code
+        WHERE  ad.document->>'registration_date' BETWEEN $1 AND $2
+          AND ad.document ->> 'assessment_type' = $3
+          AND ad.document ->> 'transaction_type' = $4
+          AND ad.document ->> 'postcode' NOT LIKE $5
+          AND ((ad.document -> 'main_heating')::varchar  ILIKE $6 OR (ad.document -> 'main_heating')::varchar ILIKE $7)
         GROUP BY onsn.name;
       SQL
 
@@ -81,16 +80,16 @@ module Gateway
 
     def fetch_by_parliamentary_constituency(start_date:, end_date:)
       sql = <<~SQL
-              SELECT onsn.name as westminster_parliamentary_constituency,
-                    COUNT(DISTINCT assessment_id) as number_of_assessments
-                FROM assessment_documents ad
-                left join ons_postcode_directory ons on ad.document ->> 'postcode' = ons.postcode
-                left join ons_postcode_directory_names onsn on ons.westminster_parliamentary_constituency_code = onsn.area_code
-         WHERE  ad.document->>'registration_date' BETWEEN $1 AND $2
-        AND ad.document ->> 'assessment_type' = $3
-        AND ad.document ->> 'transaction_type' = $4
-        AND ad.document ->> 'postcode' NOT LIKE $5
-        AND ((ad.document -> 'main_heating')::varchar ILIKE $6 or (ad.document -> 'main_heating')::varchar ILIKE $7)
+        SELECT onsn.name AS westminster_parliamentary_constituency,
+               COUNT(DISTINCT assessment_id) AS number_of_assessments
+        FROM assessment_documents ad
+          LEFT JOIN ons_postcode_directory ons ON ad.document ->> 'postcode' = ons.postcode
+          LEFT JOIN ons_postcode_directory_names onsn ON ons.westminster_parliamentary_constituency_code = onsn.area_code
+        WHERE  ad.document->>'registration_date' BETWEEN $1 AND $2
+          AND ad.document ->> 'assessment_type' = $3
+          AND ad.document ->> 'transaction_type' = $4
+          AND ad.document ->> 'postcode' NOT LIKE $5
+          AND ((ad.document -> 'main_heating')::varchar ILIKE $6 OR (ad.document -> 'main_heating')::varchar ILIKE $7)
         GROUP BY onsn.name;
       SQL
 
@@ -100,45 +99,45 @@ module Gateway
     def fetch_by_description(start_date:, end_date:)
       sql = <<~SQL
         SELECT
-             count(distinct assessment_id) as number_of_assessments,
-                CASE
-                    WHEN main_heating_description ILIKE '%Mixed exhaust air source heat pump%' THEN 'Mixed exhaust air source heat pump'
-                    WHEN main_heating_description ILIKE '%Exhaust air MEV source heat pump%' THEN 'Exhaust air MEV source heat pump'
-                    WHEN main_heating_description ILIKE '%Ground source heat pump%' THEN 'Ground source heat pump'
-                    WHEN ((main_heating_description ILIKE '%Pwmp gwres%') AND (main_heating_description ILIKE '%ddaear%'))  THEN 'Ground source heat pump'
-                    WHEN main_heating_description ILIKE '%Water source%' THEN 'Water source heat pump'
-                    WHEN main_heating_description ILIKE '%Solar assisted%' THEN 'Solar assisted heat pump'
-                    WHEN main_heating_description ILIKE '%Electric heat pump%' THEN 'Electric heat pump'
-                    WHEN main_heating_description ILIKE '%Community%' THEN 'Community heat pump'
-                    WHEN main_heating_description ILIKE '%Exhaust source%' THEN 'Exhaust source heat pump'
-                    WHEN main_heating_description  ILIKE '%Air source heat pump%' THEN 'Air source heat pump'
-                    WHEN ((main_heating_description ILIKE '%Pwmp gwres%') AND (main_heating_description ILIKE '%awyr%'))  THEN 'Air source heat pump'
-                    ELSE main_heating_description
-                END as heat_pump_description
+          COUNT(DISTINCT assessment_id) AS number_of_assessments,
+          CASE
+            WHEN main_heating_description ILIKE '%Mixed exhaust air source heat pump%' THEN 'Mixed exhaust air source heat pump'
+            WHEN main_heating_description ILIKE '%Exhaust air MEV source heat pump%' THEN 'Exhaust air MEV source heat pump'
+            WHEN main_heating_description ILIKE '%Ground source heat pump%' THEN 'Ground source heat pump'
+            WHEN ((main_heating_description ILIKE '%Pwmp gwres%') AND (main_heating_description ILIKE '%ddaear%'))  THEN 'Ground source heat pump'
+            WHEN main_heating_description ILIKE '%Water source%' THEN 'Water source heat pump'
+            WHEN main_heating_description ILIKE '%Solar assisted%' THEN 'Solar assisted heat pump'
+            WHEN main_heating_description ILIKE '%Electric heat pump%' THEN 'Electric heat pump'
+            WHEN main_heating_description ILIKE '%Community%' THEN 'Community heat pump'
+            WHEN main_heating_description ILIKE '%Exhaust source%' THEN 'Exhaust source heat pump'
+            WHEN main_heating_description  ILIKE '%Air source heat pump%' THEN 'Air source heat pump'
+            WHEN ((main_heating_description ILIKE '%Pwmp gwres%') AND (main_heating_description ILIKE '%awyr%'))  THEN 'Air source heat pump'
+            ELSE main_heating_description
+          END AS heat_pump_description
         FROM (
         SELECT assessment_id as assessment_id,
-               (jsonb_array_elements(ad.document -> ('main_heating')) ->> 'description')::varchar as main_heating_description
-            FROM assessment_documents ad
-            WHERE ad.document->>'registration_date' BETWEEN $1 AND $2
-            AND ad.document ->> 'assessment_type' = 'SAP'
-            AND ad.document ->> 'postcode' NOT LIKE 'BT%'
-            AND ad.document ->> 'transaction_type' = '6') as main_heating_query
+               (jsonb_array_elements(ad.document -> ('main_heating')) ->> 'description')::varchar AS main_heating_description
+        FROM assessment_documents ad
+        WHERE ad.document->>'registration_date' BETWEEN $1 AND $2
+          AND ad.document ->> 'assessment_type' = 'SAP'
+          AND ad.document ->> 'postcode' NOT LIKE 'BT%'
+          AND ad.document ->> 'transaction_type' = '6') AS main_heating_query
         WHERE ((main_heating_description ILIKE '%heat pump%') OR (main_heating_description ILIKE '%pwmp gwres%'))
         GROUP BY
-            CASE
-                    WHEN main_heating_description ILIKE '%Mixed exhaust air source heat pump%' THEN 'Mixed exhaust air source heat pump'
-                    WHEN main_heating_description ILIKE '%Exhaust air MEV source heat pump%' THEN 'Exhaust air MEV source heat pump'
-                    WHEN main_heating_description ILIKE '%Ground source heat pump%' THEN 'Ground source heat pump'
-                    WHEN ((main_heating_description ILIKE '%Pwmp gwres%') AND (main_heating_description ILIKE '%ddaear%'))  THEN 'Ground source heat pump'
-                    WHEN main_heating_description ILIKE '%Water source%' THEN 'Water source heat pump'
-                    WHEN main_heating_description ILIKE '%Solar assisted%' THEN 'Solar assisted heat pump'
-                    WHEN main_heating_description ILIKE '%Electric heat pump%' THEN 'Electric heat pump'
-                    WHEN main_heating_description ILIKE '%Community%' THEN 'Community heat pump'
-                    WHEN main_heating_description ILIKE '%Exhaust source%' THEN 'Exhaust source heat pump'
-                    WHEN main_heating_description  ILIKE '%Air source heat pump%' THEN 'Air source heat pump'
-                    WHEN ((main_heating_description ILIKE '%Pwmp gwres%') AND (main_heating_description ILIKE '%awyr%'))  THEN 'Air source heat pump'
-                    ELSE main_heating_description
-                END
+          CASE
+            WHEN main_heating_description ILIKE '%Mixed exhaust air source heat pump%' THEN 'Mixed exhaust air source heat pump'
+            WHEN main_heating_description ILIKE '%Exhaust air MEV source heat pump%' THEN 'Exhaust air MEV source heat pump'
+            WHEN main_heating_description ILIKE '%Ground source heat pump%' THEN 'Ground source heat pump'
+            WHEN ((main_heating_description ILIKE '%Pwmp gwres%') AND (main_heating_description ILIKE '%ddaear%'))  THEN 'Ground source heat pump'
+            WHEN main_heating_description ILIKE '%Water source%' THEN 'Water source heat pump'
+            WHEN main_heating_description ILIKE '%Solar assisted%' THEN 'Solar assisted heat pump'
+            WHEN main_heating_description ILIKE '%Electric heat pump%' THEN 'Electric heat pump'
+            WHEN main_heating_description ILIKE '%Community%' THEN 'Community heat pump'
+            WHEN main_heating_description ILIKE '%Exhaust source%' THEN 'Exhaust source heat pump'
+            WHEN main_heating_description  ILIKE '%Air source heat pump%' THEN 'Air source heat pump'
+            WHEN ((main_heating_description ILIKE '%Pwmp gwres%') AND (main_heating_description ILIKE '%awyr%'))  THEN 'Air source heat pump'
+            ELSE main_heating_description
+          END
       SQL
 
       ActiveRecord::Base.connection.exec_query(sql, "SQL", bindings(start_date:, end_date:).first(2)).map { |result| result }
