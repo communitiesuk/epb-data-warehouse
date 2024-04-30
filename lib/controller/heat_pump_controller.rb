203cb849d1c6eb60bd7304f4e9a51371104ab43a
Alice Lui <54395483+al01010@users.noreply.github.com>
2024-04-30 11:08:12 +0100

    Remove OAuth scope from heat pump count by floor area endpoint

--- lib/controller/heat_pump_controller.rb
+++ lib/controller/heat_pump_controller.rb
@@ -1,7 +1,6 @@
 module Controller
   class HeatPumpController < Controller::BaseController
-    get "/api/heat-pump-counts/floor-area",
-        auth_token_has_all: %w[warehouse:read] do
+    get "/api/heat-pump-counts/floor-area" do
       use_case = Container.get_heat_pump_counts_by_floor_area_use_case
       result = use_case.execute(start_date: "2022-05-01", end_date: "2023-05-31")
       json_api_response code: 200, data: result
