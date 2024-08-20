ce16d5f0e041555c619cbf174bcd337844b88782
Eleanor Mollett <eleanor.mollett@gmail.com>
2024-08-20 16:19:53 +0100

    Update ADD queries to use country code instead of postcode

--- psql/part_L_pv_lzc_data.sql
+++ psql/part_L_pv_lzc_data.sql
@@ -2,12 +2,12 @@
 
 -- sap (domestic part-L)
 SELECT document ->> 'hashed_assessment_id' as assessment_id,
-       document ->> 'schema_type' as schema_type
+    document ->> 'schema_type' as schema_type
 FROM assessment_documents
 WHERE document ->> 'schema_type' IN ('SAP-Schema-19.0.0', 'SAP-Schema-19.1.0')
-AND document ->> 'postcode' NOT LIKE 'BT%'
-AND document->>'registration_date' between '2023-07-01' and '2023-09-30'
-AND document -> 'opt_out' IS NULL;
+  AND document ->> 'country_code' IN ('ENG', 'WLS', 'EAW')
+  AND document->>'registration_date' between '2024-04-01' and '2024-06-31'
+  AND document -> 'opt_out' IS NULL;
 
 -- lzc
 SELECT
@@ -16,11 +16,11 @@ SELECT
     ad.document ->> 'schema_type' as schema_type,
     ad.document ->> 'lzc_energy_sources' as lzc_energy_sources
 FROM assessment_documents ad
-WHERE ad.document->>'registration_date' between '2023-07-01' and '2023-09-30'
-AND ad.document ->> 'lzc_energy_sources' IS NOT NULL
-AND (ad.document ->> 'assessment_type')::varchar = 'SAP'
-AND ad.document -> 'opt_out' IS NULL
-AND ad.document ->> 'postcode' NOT LIKE 'BT%';
+WHERE ad.document->>'registration_date' between '2024-04-01' and '2024-06-31'
+  AND ad.document ->> 'lzc_energy_sources' IS NOT NULL
+  AND (ad.document ->> 'assessment_type')::varchar = 'SAP'
+  AND ad.document -> 'opt_out' IS NULL
+  AND ad.document ->> 'country_code' IN ('ENG', 'WLS', 'EAW');
 
 -- pv arrays
 SELECT
@@ -28,8 +28,8 @@ SELECT
     ad.document ->> 'registration_date' as registration_date,
     pv_arrays ->> 'peak_power' as peak_power
 FROM assessment_documents ad,
-jsonb_array_elements(ad.document -> 'sap_energy_source' -> 'pv_arrays') pv_arrays
-WHERE ad.document->>'registration_date' between '2023-07-01' and '2023-09-30'
-AND (ad.document ->> 'assessment_type')::varchar = 'SAP'
-AND ad.document ->> 'postcode' NOT LIKE 'BT%'
-AND ad.document -> 'opt_out' IS NULL;
+    jsonb_array_elements(ad.document -> 'sap_energy_source' -> 'pv_arrays') pv_arrays
+WHERE ad.document->>'registration_date' between '2024-04-01' and '2024-06-31'
+  AND (ad.document ->> 'assessment_type')::varchar = 'SAP'
+  AND ad.document ->> 'country_code' IN ('ENG', 'WLS', 'EAW')
+  AND ad.document -> 'opt_out' IS NULL;
