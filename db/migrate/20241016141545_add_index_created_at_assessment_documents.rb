b172a551b8278a14dc602d619c308c09a36189e6
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2024-10-16 15:13:52 +0100

    EPBR-6992 update update_created_at_eav rake to use distinct for upsert

--- db/migrate/20241016141545_add_index_created_at_assessment_documents.rb
+++ db/migrate/20241016141545_add_index_created_at_assessment_documents.rb
@@ -1,8 +1,8 @@
 class AddIndexCreatedAtAssessmentDocuments < ActiveRecord::Migration[7.0]
   def self.up
     add_index :assessment_documents, "(document->>'created_At')", using: :btree, name: "index_document_created_At"
-
   end
+
   def self.down
     execute "DROP INDEX IF EXISTS index_document_created_At"
   end
