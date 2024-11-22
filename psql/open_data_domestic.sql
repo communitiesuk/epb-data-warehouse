13cc8fc732bda04f8e94dfa81f11e6a8c96d1862
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2024-11-22 16:23:09 +0000

    move get_lookup_value function to new file
    update the functions to deal with issue of SAP schemas having '/SAP' in the version

--- psql/open_data_domestic.sql
+++ psql/open_data_domestic.sql
@@ -44,20 +44,3 @@ WHERE full_address LIKE '%london%'
 
 
 
-CREATE FUNCTION get_lookup_value(attribute varchar, key varchar, assessmennt_type varchar, version varchar) returns character varying
-    language plpgsql
-as
-$$
-DECLARE
-    value varchar;
-BEGIN
-
-    SELECT DISTINCT lookup_value INTO value
-    FROM assessment_attribute_lookups aal
-             JOIN assessment_lookups al on aal.lookup_id = al.id
-             JOIN public.assessment_attributes aa on aal.attribute_id = aa.attribute_id
-    WHERE  aa.attribute_name = attribute AND al.lookup_key = key AND aal.type_of_assessment =assessmennt_type and schema_version=version;
-
-    RETURN value;
-
-END $$
