0512f5a74154f91ad49bfddd70303f94dafe8efc
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2024-12-17 10:57:10 +0000

    Adjust gateway to handle additional  arguments

--- lib/gateway/domestic_search_gateway.rb
+++ lib/gateway/domestic_search_gateway.rb
@@ -1,12 +1,43 @@
 module Gateway
   class DomesticSearchGateway
-    def fetch
+    def fetch(date_start:, date_end:, row_limit: 5000, council: nil)
+      bindings = [
+        ActiveRecord::Relation::QueryAttribute.new(
+          "date_start",
+          date_start,
+          ActiveRecord::Type::String.new,
+        ),
+        ActiveRecord::Relation::QueryAttribute.new(
+          "date_end",
+          date_end,
+          ActiveRecord::Type::String.new,
+        ),
+        ActiveRecord::Relation::QueryAttribute.new(
+          "limit",
+          row_limit,
+          ActiveRecord::Type::Integer.new,
+        ),
+      ]
+
       sql = <<-SQL
         SELECT *
-        FROM mvw_domestic_search
+        FROM mvw_domestic_search#{' '}
+        WHERE LODGEMENT_DATE BETWEEN $1 AND $2
       SQL
 
-      ActiveRecord::Base.connection.exec_query(sql, "SQL").map { |result| result }
+      unless council.nil?
+        sql += " AND local_authority_label = $4"
+
+        bindings << ActiveRecord::Relation::QueryAttribute.new(
+          "council",
+          council,
+          ActiveRecord::Type::String.new,
+        )
+      end
+
+      sql += " LIMIT $3"
+
+      ActiveRecord::Base.connection.exec_query(sql, "SQL", bindings).map { |result| result }
     end
   end
 end
--- spec/unit/gateway/domestic_search_gateway_spec.rb
+++ spec/unit/gateway/domestic_search_gateway_spec.rb
@@ -3,6 +3,8 @@ require_relative "../../shared_context/shared_ons_data"
 
 describe Gateway::DomesticSearchGateway do
   let(:gateway) { described_class.new }
+  let(:date_start) { "2021-12-01" }
+  let(:date_end) { "2023-12-09" }
 
   include_context "when lodging XML"
   include_context "when saving ons data"
@@ -21,40 +23,69 @@ describe Gateway::DomesticSearchGateway do
       type_of_assessment = "SAP"
       schema_type = "SAP-Schema-19.0.0"
       add_countries
-      add_assessment(assessment_id: "0000-0000-0000-0000-0000", schema_type:, type_of_assessment:)
-      add_assessment(assessment_id: "0000-0000-0000-0000-0001", schema_type:, type_of_assessment:)
+      add_assessment(assessment_id: "0000-0000-0000-0000-0000", schema_type:, type_of_assessment:, different_fields: {
+        "postcode": "W6 9ZD",
+      })
+      add_assessment(assessment_id: "0000-0000-0000-0000-0001", schema_type:, type_of_assessment:, different_fields: {
+        "postcode": "SW10 0AA",
+      })
       add_assessment(assessment_id: "0000-0000-0000-0000-0003", schema_type:, type_of_assessment:, different_fields: {
         "postcode": "BT1 1AA",
+      })
+      add_assessment(assessment_id: "0000-0000-0000-0000-0004", schema_type:, type_of_assessment:, different_fields: {
+        "registration_date": "2024-12-06", "postcode": "SW10 0AA"
+      })
+      add_assessment(assessment_id: "0000-0000-0000-0000-0005", schema_type: "CEPC-8.0.0", type_of_assessment: "CEPC", type: "cepc", different_fields: {
+        "postcode": "W6 9ZD",
       })
       Gateway::MaterializedViewsGateway.new.refresh(name: "mvw_domestic_search")
     end
 
     let(:expected_data) do
-      { "rrn" => "0000-0000-0000-0000-0000", "address1" => "1 Some Street", "address2" => "Some Area", "address3" => "Some County", "postcode" => "A0 0AA", "total_floor_area" => "165", "current_energy_rating" => "72", "lodgement_date" => "2022-05-09" }
+      { "rrn" => "0000-0000-0000-0000-0000", "address1" => "1 Some Street", "address2" => "Some Area", "address3" => "Some County", "postcode" => "W6 9ZD", "total_floor_area" => "165", "current_energy_rating" => "72", "lodgement_date" => "2022-05-09" }
     end
 
     it "returns a rows for each assessment in England & Wales" do
-      expect(gateway.fetch.length).to eq 2
-      expect(gateway.fetch[0]["rrn"]).to eq "0000-0000-0000-0000-0000"
-      expect(gateway.fetch[1]["rrn"]).to eq "0000-0000-0000-0000-0001"
+      expect(gateway.fetch(date_start:, date_end:).length).to eq 2
+      expect(gateway.fetch(date_start:, date_end:)[0]["rrn"]).to eq "0000-0000-0000-0000-0000"
+      expect(gateway.fetch(date_start:, date_end:)[1]["rrn"]).to eq "0000-0000-0000-0000-0001"
     end
 
     it "returns expected values for the first row" do
-      expect(gateway.fetch[0]).to match a_hash_including expected_data
+      expect(gateway.fetch(date_start:, date_end:)[0]).to match a_hash_including expected_data
     end
 
     it "translates enum values into strings using the user defined function" do
-      expect(gateway.fetch[0]["transaction_type"]).to eq "New dwelling"
-      expect(gateway.fetch[0]["property_type"]).to eq "House"
+      expect(gateway.fetch(date_start:, date_end:)[0]["transaction_type"]).to eq "New dwelling"
+      expect(gateway.fetch(date_start:, date_end:)[0]["property_type"]).to eq "House"
     end
 
     context "when filtering by council" do
+      let(:council) { "Hammersmith and Fulham" }
+
+      it "returns data with a corresponding council" do
+        expect(gateway.fetch(date_start:, date_end:, council:).length).to eq(2)
+        expect(gateway.fetch(date_start:, date_end:, council:)[0]["rrn"]).to eq("0000-0000-0000-0000-0000")
+        expect(gateway.fetch(date_start:, date_end:, council:)[1]["rrn"]).to eq("0000-0000-0000-0000-0001")
+      end
     end
 
     context "when filtering by a date range" do
+      let(:date_start) { "2024-12-01" }
+      let(:date_end) { "2024-12-09" }
+
+      it "returns the row with a relevant data" do
+        expect(gateway.fetch(date_start:, date_end:).length).to eq(1)
+        expect(gateway.fetch(date_start:, date_end:)[0]["rrn"]).to eq("0000-0000-0000-0000-0004")
+      end
     end
 
     context "when limiting by number of row" do
+      let(:row_limit) { 1 }
+
+      it "returns 1" do
+        expect(gateway.fetch(date_start:, date_end:, row_limit:).length).to eq(1)
+      end
     end
   end
 end
