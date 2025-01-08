d7842d8d8222fc2cb1212a056eed773fb1fcf7cc
Pedro Alvarez <pedro.alvarez@madetech.com>
2025-01-08 11:31:33 +0000

    Create initial domestic search mvw test

--- spec/unit/gateway/domestic_search_gateway_spec.rb
+++ spec/unit/gateway/domestic_search_gateway_spec.rb
@@ -89,5 +89,59 @@ describe Gateway::DomesticSearchGateway do
         expect(gateway.fetch(date_start:, date_end:, row_limit:).length).to eq(1)
       end
     end
+
+    context "when creating the materialized view" do
+      let(:expected_data) do
+        { "rrn" => "0000-0000-0000-0000-0001",
+          "address1" => "1 Some Street",
+          "address2" => "Some Area",
+          "address3" => "Some County",
+          "postcode" => "SW10 0AA",
+          "total_floor_area" => "165",
+          "current_energy_rating" => "72",
+          "lodgement_date" => "2022-05-09",
+          "local_authority_label" => "Hammersmith and Fulham",
+          "constituency_label" => "Chelsea and Fulham",
+          "transaction_type" => "New dwelling",
+          "property_type" => "House",
+          "full_address" => "1 some street some area some county whitbury" }
+      end
+
+      let(:first_query_result) do
+        ActiveRecord::Base.connection.exec_query(
+          "SELECT
+    ad.assessment_id as RRN,
+    document ->> 'address_line_1' as ADDRESS1,
+    document ->> 'address_line_2' as ADDRESS2,
+    document ->> 'address_line_3' as ADDRESS3,
+    document ->> 'postcode' as POSTCODE,
+    document ->> 'total_floor_area' as TOTAL_FLOOR_AREA,
+    document->>'energy_rating_current' as CURRENT_ENERGY_RATING,
+    document->>'registration_date' as LODGEMENT_DATE,
+    os_la.name as LOCAL_AUTHORITY_LABEL,
+    os_p.name as CONSTITUENCY_LABEL,
+    get_lookup_value('transaction_type', document ->> 'transaction_type', document ->> 'assessment_type', document->> 'schema_type' ) as TRANSACTION_TYPE,
+    get_lookup_value('property_type', document ->> 'property_type', document ->> 'assessment_type', document->> 'schema_type' ) as PROPERTY_TYPE,
+
+
+    LOWER(CONCAT_WS(' ', (document ->> 'address_line_1')::varchar,
+                    (document ->> 'address_line_2')::varchar,
+                    (document ->> 'address_line_3')::varchar,
+                    (document ->> 'post_town')::varchar)) as full_address
+FROM assessment_documents ad
+join assessments_country_ids aci on ad.assessment_id = aci.assessment_id
+join countries co on aci.country_id = co.country_id
+left JOIN ons_postcode_directory ons on document ->> 'postcode' = ons.postcode
+left join ons_postcode_directory_names os_la on ons.local_authority_code = os_la.area_code
+left join ons_postcode_directory_names os_p on ons.westminster_parliamentary_constituency_code = os_p.area_code
+WHERE document ->> 'assessment_type' IN ('RdSAP', 'SAP')
+  AND co.country_code IN ('EAW', 'ENG', 'WLS')",
+        ).first
+      end
+
+      it "creates a table with the required data" do
+        expect(first_query_result).to eq expected_data
+      end
+    end
   end
 end
