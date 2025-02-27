95c6db019694c2eb1afef69048aa7895e0b6e82d
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2025-02-27 14:09:01 +0000

    OD-26 Adds region to commercial search mview

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_26_142015) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_27_114215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -74,7 +74,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_26_142015) do
   create_table "audit_logs", force: :cascade do |t|
     t.string "assessment_id"
     t.string "event_type", null: false
-    t.datetime "timestamp", default: "2025-02-26 15:06:59", null: false
+    t.datetime "timestamp", default: "2025-02-27 12:21:12", null: false
 
     t.unique_constraint ["assessment_id", "event_type"], name: "idx_audit_log_rrn_event"
   end
