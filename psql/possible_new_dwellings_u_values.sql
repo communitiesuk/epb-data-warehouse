289afa13d301578463a03216dac23912cb23f8be
Alice Lui <54395483+al01010@users.noreply.github.com>
2025-03-28 10:53:36 +0000

    EPBR-8907: Update PSQL Script for an ADD Data Request

--- psql/possible_new_dwellings_u_values.sql
+++ psql/possible_new_dwellings_u_values.sql
@@ -1,10 +1,11 @@
--- for newer assessments
+-- for newer assessments (updated Mar 2025)
 
 select
 ado.assessment_id as assessment_id,
 ado.document ->> 'hashed_assessment_id' hashed_assessment_id,
-c.country_name as country_name,
 ado.document ->> 'schema_type' schema_type,
+ado.document ->> 'registration_date' as registration_date,
+ado.document ->> 'country_code' as country_code,
 u_value,
 ado.document ->> 'transaction_type' transaction_type,
 ado.document ->'sap_ventilation'-> 'pressure_test' pressure_test_code,
@@ -21,16 +22,14 @@ total_wall_area
 from
 assessment_documents ad,
 jsonb_to_recordset(ad.document -> 'sap_building_parts' -> 0 -> 'sap_walls') as wall(wall_type int, u_value float, total_wall_area float)
-where ad.document->>'registration_date' < '2024-11-01'
+where ad.document->>'registration_date' between '2025-01-01' and '2025-01-31'
 AND (ad.document ->> 'schema_type')::varchar IN ('SAP-Schema-19.0.0', 'SAP-Schema-19.1.0')
 and wall_type in (2)
 )
 as u_value_query
 group by assessment_id) as sample
 inner join assessment_documents ado on sample.assessment_id = ado.assessment_id
-join assessments_country_ids aci on sample.assessment_id = aci.assessment_id
-join countries c on c.country_id = aci.country_id
-where aci.country_id in (1, 2, 4);
+where ado.document ->> 'country_code' IN ('ENG', 'WLS', 'EAW');
 
 -- for older assessments
 
