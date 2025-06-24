a090a3cbdf730d458ebd8cde3e1a019dfac1a19c
Rami Abusamra <ramimilkyasir@gmail.com>
2025-06-24 14:26:53 +0100

    OD-152 Updates domestic search filter
    Updates filter in predicate to use a join to temporary table

--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_20_104721) do
   create_table "audit_logs", force: :cascade do |t|
     t.string "assessment_id"
     t.string "event_type", null: false
-    t.datetime "timestamp", default: "2025-06-23 13:31:07", null: false
+    t.datetime "timestamp", default: "2025-06-24 10:52:46", null: false
 
     t.unique_constraint ["assessment_id", "event_type"], name: "idx_audit_log_rrn_event"
   end
--- lib/gateway/base_search_gateway.rb
+++ lib/gateway/base_search_gateway.rb
@@ -65,19 +65,6 @@ module Gateway
         end
       end
 
-      arr.concat [
-        ActiveRecord::Relation::QueryAttribute.new(
-          "date_start",
-          this_args[:date_start],
-          ActiveRecord::Type::Date.new,
-        ),
-        ActiveRecord::Relation::QueryAttribute.new(
-          "date_end",
-          this_args[:date_end],
-          ActiveRecord::Type::Date.new,
-        ),
-      ]
-
       unless this_args[:council].nil?
         this_args[:council].each_with_index do |council, idx|
           arr << ActiveRecord::Relation::QueryAttribute.new(
@@ -97,6 +84,19 @@ module Gateway
           )
         end
       end
+
+      arr.concat [
+        ActiveRecord::Relation::QueryAttribute.new(
+          "date_start",
+          this_args[:date_start],
+          ActiveRecord::Type::Date.new,
+        ),
+        ActiveRecord::Relation::QueryAttribute.new(
+          "date_end",
+          this_args[:date_end],
+          ActiveRecord::Type::Date.new,
+        ),
+      ]
 
       unless this_args[:postcode].nil?
         arr << ActiveRecord::Relation::QueryAttribute.new(
@@ -131,23 +131,25 @@ module Gateway
         index += this_args[:eff_rating].size
       end
 
-      sql << (sql.include?("WHERE") ? " AND " : " WHERE ")
-      sql << "lodgement_date BETWEEN $#{index} AND $#{index + 1}"
-      index += 2
-
       unless this_args[:council].nil?
-        sql << " AND local_authority_label IN ("
-        sql << this_args[:council].each_with_index.map { |_, idx| "$#{index + idx}" }.join(", ")
-        sql << ")"
+        sql << " JOIN ( VALUES "
+        sql << this_args[:council].each_with_index.map { |_, idx| "($#{index + idx})" }.join(", ")
+        sql << ") councils (c) "
+        sql << "ON (local_authority_label = c)"
         index += this_args[:council].size
       end
 
       unless this_args[:constituency].nil?
-        sql << " AND constituency_label IN ("
-        sql << this_args[:constituency].each_with_index.map { |_, idx| "$#{index + idx}" }.join(", ")
-        sql << ")"
+        sql << " JOIN ( VALUES "
+        sql << this_args[:constituency].each_with_index.map { |_, idx| "($#{index + idx})" }.join(", ")
+        sql << ") cons (d) "
+        sql << "ON (constituency_label = d)"
         index += this_args[:constituency].size
       end
+
+      sql << (sql.include?("WHERE") ? " AND " : " WHERE ")
+      sql << "lodgement_date BETWEEN $#{index} AND $#{index + 1}"
+      index += 2
 
       unless this_args[:postcode].nil?
         sql << " AND postcode = $#{index}"
