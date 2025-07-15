ecf8efed17278b07987eaf2ac0001893fe208079
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2025-07-15 12:13:59 +0100

    Add constituencies to OnsPostcodeDirectoryNamesGateway
    This data point will be cached as called into the init of the class

--- lib/gateway/ons_postcode_directory_names_gateway.rb
+++ lib/gateway/ons_postcode_directory_names_gateway.rb
@@ -1,16 +1,29 @@
 module Gateway
   class OnsPostcodeDirectoryNamesGateway
+    attr_reader :councils, :constituencies
+
     def initialize
+      @data = fetch
       @councils = fetch_councils
+      @constituencies = fetch_constituencies
     end
 
   private
 
     def fetch_councils
+      @data = fetch.select { |i| i["type"] == "Local authority" }
+    end
+
+    def fetch_constituencies
+      @data = fetch.select { |i| i["type"] == "Westminster parliamentary constituency" }
+    end
+
+    def fetch
       sql = <<-SQL
-        SELECT name, id
+       SELECT name, id, type
         FROM ons_postcode_directory_names
-        WHERE type = 'Local authority'
+
+        WHERE type in ('Local authority', 'Westminster parliamentary constituency')
       SQL
       ActiveRecord::Base.connection.exec_query(sql).map { |result| result }
     end
--- spec/unit/gateway/ons_postcode_directory_names_gateway_spec.rb
+++ spec/unit/gateway/ons_postcode_directory_names_gateway_spec.rb
@@ -10,4 +10,24 @@ describe Gateway::OnsPostcodeDirectoryNamesGateway do
       expect { gateway }.not_to raise_error
     end
   end
+
+  describe "councils" do
+    let(:councils) do
+      ["Hammersmith and Fulham", "South Lanarkshire", "Belfast", "Westminster"]
+    end
+
+    it "returns a list of councils" do
+      expect(gateway.councils.map { |i| i["name"] }).to eq councils
+    end
+  end
+
+  describe "constituencies" do
+    let(:constituencies) do
+      ["Belfast South", "Chelsea and Fulham", "Lanark and Hamilton East", "Cities of London and Westminster"]
+    end
+
+    it "returns a list of councils" do
+      expect(gateway.constituencies.map { |i| i["name"] }).to eq constituencies
+    end
+  end
 end
