f7ae51e9d9d240aead69147cae6ead81a50caca5
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-07-21 14:46:21 +0100

    OD-193: Update data points for floor_level and main_heating_controls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_16_153536) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_18_114137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -102,7 +102,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_16_153536) do
   create_table "audit_logs", force: :cascade do |t|
     t.string "assessment_id"
     t.string "event_type", null: false
-    t.datetime "timestamp", default: "2025-07-17 09:17:23", null: false
+    t.datetime "timestamp", default: "2025-07-18 14:55:32", null: false
 
     t.unique_constraint ["assessment_id", "event_type"], name: "idx_audit_log_rrn_event"
   end
