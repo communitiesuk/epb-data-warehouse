9a5fad8e6ecc1834d36d1a615363ad1f94fab1c9
Pedro Alvarez <pedro.alvarez@madetech.com>
2025-07-28 16:01:42 +0100

    Fix sum_attribute_values SQL function for PG 17

---
+++ db/migrate/20250728155229_fix_create_sum_attribute_values.rb
@@ -0,0 +1,21 @@
+class FixCreateSumAttributeValues < ActiveRecord::Migration[7.0]
+  def self.up
+    execute "
+      CREATE OR REPLACE FUNCTION sum_attribute_values(attribute_names VARCHAR[], assessment_id VARCHAR)
+      RETURNS numeric
+      LANGUAGE plpgsql
+      AS $$
+      DECLARE
+        attribute_name VARCHAR;
+        total_value NUMERIC := 0;
+      BEGIN
+        FOREACH attribute_name IN ARRAY attribute_names LOOP
+          total_value := total_value + public.get_attribute_value(attribute_name, assessment_id)::numeric;
+        END LOOP;
+        RETURN total_value;
+      END $$;
+    "
+  end
+
+  def self.down; end
+end
