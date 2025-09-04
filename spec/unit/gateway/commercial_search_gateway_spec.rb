33178659a26d46a719027b66b1711edfb3639e40
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-09-04 13:29:57 +0100

    OD-256: Updates commercial mvw data points

--- spec/unit/gateway/commercial_search_gateway_spec.rb
+++ spec/unit/gateway/commercial_search_gateway_spec.rb
@@ -25,33 +25,34 @@ describe Gateway::CommercialSearchGateway do
 
       type_of_assessment = "CEPC"
 
-      add_assessment_eav(assessment_id: "0000-0000-0000-0000-0006", schema_type: "CEPC-8.0.0", type_of_assessment:, type: "cepc", different_fields: {
-        "postcode" => "SW10 0AA",
+      add_assessment_eav(assessment_id: "0000-0000-0000-0000-0006", schema_type: "CEPC-8.0.0", type_of_assessment:, type: "cepc", add_to_assessment_search: true, different_fields: {
+        "postcode" => "SW10 0AA", "country_id": 1
       })
-      add_assessment_eav(assessment_id: "0000-0000-0000-0000-0007", schema_type: "CEPC-7.0", type_of_assessment:, type: "cepc+rr", different_fields: {
-        "postcode" => "SW10 0AA",
+      add_assessment_eav(assessment_id: "0000-0000-0000-0000-0007", schema_type: "CEPC-7.0", type_of_assessment:, type: "cepc+rr", add_to_assessment_search: true, different_fields: {
+        "postcode" => "SW10 0AA", "country_id": 1
       })
-      add_assessment(assessment_id: "0000-0000-0000-0000-0001", schema_type: "SAP-Schema-19.0.0", type_of_assessment: "SAP", different_fields: {
-        "postcode": "SW10 0AA",
+      add_assessment_eav(assessment_id: "0000-0000-0000-0000-0001", schema_type: "SAP-Schema-19.0.0", type_of_assessment: "SAP", add_to_assessment_search: true, different_fields: {
+        "postcode": "SW10 0AA", "country_id": 2
       })
       import_look_ups(schema_versions: %w[CEPC-8.0.0 CEPC-7.0])
       Gateway::MaterializedViewsGateway.new.refresh(name: "mvw_commercial_search")
     end
 
     let(:cepc_expected_data) do
-      { "rrn" => "0000-0000-0000-0000-0006",
+      { "certificate_number" => "0000-0000-0000-0000-0006",
         "estimated_aircon_kw_rating" => "3",
         "address1" => "60 Maple Syrup Road",
         "address2" => "Candy Mountain",
         "address3" => nil,
-        "region" => "London",
         "postcode" => "SW10 0AA",
         "posttown" => "Big Rock",
         "property_type" => "A1/A2 Retail and Financial/Professional services",
         "asset_rating" => "84",
-        "building_reference_number" => "UPRN-000000000000",
+        "building_reference_number" => "",
         "asset_rating_band" => "D",
         "inspection_date" => "2021-03-19",
+        "local_authority" => "E09000013",
+        "local_authority_label" => "Hammersmith and Fulham",
         "lodgement_date" => "2021-03-19",
         "transaction_type" => "Mandatory issue (Marketed sale).",
         "building_level" => "3",
@@ -59,6 +60,7 @@ describe Gateway::CommercialSearchGateway do
         "main_heating_fuel" => "Grid Supplied Electricity",
         "new_build_benchmark" => "34",
         "ac_inspection_commissioned" => "4",
+        "address" => "60 Maple Syrup Road",
         "aircon_kw_rating" => "100",
         "aircon_present" => "No",
         "floor_area" => "951",
@@ -71,36 +73,43 @@ describe Gateway::CommercialSearchGateway do
         "target_emissions" => "31.05",
         "typical_emissions" => "90.98",
         "building_environment" => "Air Conditioning",
-        "country" => "England",
+        "constituency" => "E14000629",
+        "constituency_label" => "Chelsea and Fulham",
+        "county" => "",
         "primary_energy_value" => "451.27",
         "report_type" => "3",
-        "type_of_assessment" => "CEPC" }
+        "uprn" => "",
+        "uprn_source" => "" }
     end
 
     let(:cepc_rr_expected_data) do
       {
         "ac_inspection_commissioned" => "4",
         "estimated_aircon_kw_rating" => nil,
+        "address" => nil,
         "address1" => nil,
         "address2" => "Acme Coffee",
         "address3" => "13 Old Street",
-        "region" => "London",
         "posttown" => "POSTTOWN",
         "aircon_kw_rating" => "",
         "aircon_present" => "No",
-        "rrn" => "0000-0000-0000-0000-0007",
+        "building_reference_number" => "",
+        "certificate_number" => "0000-0000-0000-0000-0007",
+        "constituency" => "E14000629",
+        "constituency_label" => "Chelsea and Fulham",
+        "county" => "",
         "asset_rating" => "134",
         "asset_rating_band" => "F",
         "building_emissions" => "158.9",
         "building_environment" => "Heating and Natural Ventilation",
         "building_level" => "3",
-        "building_reference_number" => "987345673489",
-        "country" => "England",
         "existing_stock_benchmark" => "90",
         "floor_area" => "314",
         "inspection_date" => "2013-08-10",
         "lodgement_date" => "2013-08-15",
         "lodgement_datetime" => Time.parse("2013-08-15 00:00:00.000000000 +0000"),
+        "local_authority" => "E09000013",
+        "local_authority_label" => "Hammersmith and Fulham",
         "main_heating_fuel" => "Grid Supplied Electricity",
         "new_build_benchmark" => "34",
         "other_fuel_desc" => nil,
@@ -113,8 +122,9 @@ describe Gateway::CommercialSearchGateway do
         "standard_emissions" => "59.26",
         "target_emissions" => "39.95",
         "transaction_type" => "Mandatory issue (Marketed sale).",
-        "type_of_assessment" => "CEPC",
         "typical_emissions" => "106.52",
+        "uprn" => "",
+        "uprn_source" => "",
       }
     end
 
@@ -122,18 +132,25 @@ describe Gateway::CommercialSearchGateway do
       gateway.fetch(**search_arguments)
     end
 
-    it "returns a dataset with onl the 2 commercial EPCs" do
+    it "returns a dataset with only the 2 commercial EPCs" do
       expect(query_result.length).to eq 2
     end
 
     it "returns a dataset with the required data for cepc" do
-      result = query_result.find { |i| i["rrn"] == "0000-0000-0000-0000-0006" }
+      result = query_result.find { |i| i["certificate_number"] == "0000-0000-0000-0000-0006" }
       expect(result).to eq cepc_expected_data
     end
 
     it "returns a dataset with the required data for cepc+rr" do
-      result = query_result.find { |i| i["rrn"] == "0000-0000-0000-0000-0007" }
+      result = query_result.find { |i| i["certificate_number"] == "0000-0000-0000-0000-0007" }
       expect(result).to eq cepc_rr_expected_data
+    end
+
+    context "when an assessment has a certificate_number value saved into the assessment_address_id attribute" do
+      it "returns an empty value for the building_reference_number" do
+        results = gateway.fetch(**search_arguments)
+        expect(results.find { |i| i["certificate_number"] == "0000-0000-0000-0000-0006" }["building_reference_number"]).to eq ""
+      end
     end
 
     context "when checking the columns of the materialized view" do
