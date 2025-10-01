461e76227b1240e08360ea6fba42b1be887787e5
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-10-01 14:18:07 +0100

    OD-274: Adds Commercial Reports gateway
    - Added to add reports to Commercial Reports table.

---
+++ lib/gateway/commercial_reports_gateway.rb
@@ -0,0 +1,22 @@
+class Gateway::CommercialReportsGateway
+  def insert_report(assessment_id:, related_rrn:)
+    sql = <<-SQL
+            INSERT INTO commercial_reports(assessment_id, related_rrn)
+            VALUES($1, $2)
+    SQL
+
+    bindings = [
+      ActiveRecord::Relation::QueryAttribute.new(
+        "assessment_id",
+        assessment_id,
+        ActiveRecord::Type::String.new,
+      ),
+      ActiveRecord::Relation::QueryAttribute.new(
+        "related_rrn",
+        related_rrn,
+        ActiveRecord::Type::String.new,
+      ),
+    ]
+    ActiveRecord::Base.connection.exec_query(sql, "SQL", bindings)
+  end
+end
---
+++ spec/unit/gateway/commercial_reports_gateway_spec.rb
@@ -0,0 +1,33 @@
+describe Gateway::CommercialReportsGateway do
+  subject(:gateway) { described_class.new }
+
+  let(:commercial_reports_result) do
+    ActiveRecord::Base.connection.exec_query(
+      "SELECT * FROM commercial_reports",
+    )
+  end
+
+  let(:assessment_id) { "0000-0000-0000-0000-1111" }
+  let(:related_rrn) { "0000-0000-0000-0000-2222" }
+
+  describe "#insert_report" do
+    before do
+      ActiveRecord::Base.connection.exec_query("TRUNCATE TABLE commercial_reports;")
+    end
+
+    context "when saving a report" do
+      before do
+        gateway.insert_report(assessment_id:, related_rrn:)
+      end
+
+      it "saves the row to the table" do
+        expect(commercial_reports_result.length).to eq 1
+        expect(commercial_reports_result.first["assessment_id"]).to eq "0000-0000-0000-0000-1111"
+      end
+
+      it "has the correct related_rrn" do
+        expect(commercial_reports_result.first["related_rrn"]).to eq "0000-0000-0000-0000-2222"
+      end
+    end
+  end
+end
