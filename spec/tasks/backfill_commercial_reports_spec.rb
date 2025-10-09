941ccfd09ac46f81e22709c73a24b8140c99c333
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-10-09 09:53:18 +0100

    OD-276: Refactors backfill_commercial_reports.rake

--- spec/tasks/backfill_commercial_reports_spec.rb
+++ spec/tasks/backfill_commercial_reports_spec.rb
@@ -1,28 +1,14 @@
-shared_context "when inserting cepc documents" do
-  def save_new_cepc(schema:, assessment_id:, assessment_type:, sample_type:, related_rrn:, country_id: 1, created_at: nil, postcode: nil)
-    sample = Samples.xml(schema, sample_type)
-    use_case = UseCase::ParseXmlCertificate.new
-    parsed_epc = use_case.execute(xml: sample, schema_type: schema, assessment_id:)
-    parsed_epc["assessment_type"] = assessment_type
-    parsed_epc["schema_type"] = schema
-    parsed_epc["created_at"] = created_at.to_s unless created_at.nil?
-    parsed_epc["registration_date"] = created_at.to_s unless created_at.nil?
-    parsed_epc["postcode"] = postcode unless postcode.nil?
-    parsed_epc["assessment_address_id"] = "UPRN-1000000001245"
-    parsed_epc["related_rrn"] = related_rrn
-    import = Gateway::DocumentsGateway.new
-    import.add_assessment(assessment_id:, document: parsed_epc)
-    country_gateway = Gateway::AssessmentsCountryIdGateway.new
-    country_gateway.insert(assessment_id:, country_id:) unless country_id.nil?
-  end
-end
-
 require_relative "../shared_context/shared_lodgement"
+
 describe "Backfill commercial_reports table rake" do
+  before do
+    ActiveRecord::Base.connection.exec_query("TRUNCATE TABLE commercial_reports;")
+  end
+
   context "when calling the rake task" do
     subject(:task) { get_task("one_off:backfill_commercial_reports") }
 
-    include_context "when inserting cepc documents"
+    include_context "when lodging XML"
 
     let(:result) do
       ActiveRecord::Base.connection.exec_query(
@@ -30,56 +16,57 @@ describe "Backfill commercial_reports table rake" do
       )
     end
 
-    before do
-      allow($stdout).to receive(:puts)
-      ActiveRecord::Base.connection.exec_query("TRUNCATE TABLE commercial_reports;")
+    before(:all) do
+      add_countries
+
+      add_assessment_eav(assessment_id: "0000-0000-0000-0000-0001", schema_type: "CEPC-8.0.0", type_of_assessment: "CEPC", type: "cepc", add_to_assessment_search: true, different_fields: {
+        "country_id": 1, "related_rrn" => "0000-0000-0000-0000-0011"
+      })
+      add_assessment_eav(assessment_id: "0000-0000-0000-0000-0002", schema_type: "CEPC-7.0", type_of_assessment: "CEPC", type: "cepc-rr", add_to_assessment_search: true, different_fields: {
+        "related_rrn" => "0000-0000-0000-0000-0022", "country_id": 1
+      })
+      add_assessment_eav(assessment_id: "0000-0000-0000-0000-0003", schema_type: "CEPC-7.0", type_of_assessment: "DEC", type: "dec-rr", add_to_assessment_search: true, different_fields: {
+        "related_rrn" => "0000-0000-0000-0000-0033", "country_id": 1
+      })
     end
 
     context "when certificates have been saved" do
-      before do
-        save_new_cepc(schema: "CEPC-8.0.0", assessment_id: "0000-0000-0000-0000-0000", assessment_type: "CEPC", sample_type: "cepc", related_rrn: "0000-0000-0000-0000-1111")
-        save_new_cepc(schema: "CEPC-7.1", assessment_id: "1111-1111-1111-1111-1111", assessment_type: "DEC", sample_type: "dec+rr", related_rrn: "0000-0000-0000-0000-2222")
-      end
-
-      it "CEPCs are saved into commercial_reports table" do
+      it "saves DECs and CEPCs into commercial_reports table" do
         task.invoke
-        expect(result.length).to eq(2)
-      end
-
-      it "shows a message of the assessments count to be backfilled" do
-        expect { task.invoke }.to output(/Total assessments to back fill: 2/).to_stdout
+        expect(result.length).to eq(3)
       end
 
       it "inserting a duplicate assessment does not raise an error" do
         task.invoke
         task.reenable
         expect { task.invoke }.not_to raise_error
-        expect(result.length).to eq(2)
+        expect(result.length).to eq(3)
       end
 
-      it "inserting a duplicate assessment does not call the commercial reports gateway" do
-        task.invoke
+      context "when there are domestic assessments" do
+        before do
+          add_assessment_eav(assessment_id: "0000-0000-0000-0000-0004", schema_type: "SAP-Schema-19.0.0", type_of_assessment: "SAP", add_to_assessment_search: true, different_fields: {
+            "country_id": 2,
+          })
+        end
 
-        gateway_instance = instance_double(Gateway::CommercialReportsGateway)
-        allow(gateway_instance).to receive(:insert_report)
-        allow(Gateway::CommercialReportsGateway).to receive(:new).and_return(gateway_instance)
-
-        task.reenable
-        expect { task.invoke }.to output(/Total assessments to back fill: 0/).to_stdout
-        expect(gateway_instance).to have_received(:insert_report).exactly(0).times
+        it "doesn't insert invalid assessment types" do
+          task.invoke
+          expect(result.length).to eq(3)
+        end
       end
 
-      it "doesn't insert domestic assessment types" do
-        save_new_cepc(schema: "RdSAP-Schema-19.0", assessment_id: "0000-6666-4444-3333-1111", assessment_type: "RdSAP", sample_type: "epc", related_rrn: "0000-0000-0000-0000-3333")
-        save_new_cepc(schema: "SAP-Schema-17.0", assessment_id: "5555-5555-5555-5555-5555", assessment_type: "SAP", sample_type: "epc", related_rrn: "0000-0000-0000-0000-4444")
-        task.invoke
-        expect(result.length).to eq(2)
-      end
+      context "when there are valid assessments with no related_rrn" do
+        before do
+          add_assessment_eav(assessment_id: "0000-0000-0000-0000-0005", schema_type: "CEPC-8.0.0", type_of_assessment: "DEC", type: "dec-rr", add_to_assessment_search: true, different_fields: {
+            "country_id": 2, "related_rrn" => nil
+          })
+        end
 
-      it "doesn't insert assessments without related_rrn" do
-        save_new_cepc(schema: "CEPC-8.0.0", assessment_id: "2222-2222-2222-2222-2222", assessment_type: "DEC", sample_type: "dec-rr", related_rrn: nil)
-        task.invoke
-        expect(result.length).to eq(2)
+        it "doesn't insert assessments without related_rrn" do
+          task.invoke
+          expect(result.length).to eq(3)
+        end
       end
     end
   end
