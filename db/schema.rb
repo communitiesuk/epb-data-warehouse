a53b1d86c3cf95d1559abea1e3d8d3a942bf5e62
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-11-05 12:39:10 +0000

    Fix the build error.
    - We had a type in the ADDRESS3 data point in the domestic search mvw migration that was pushed in the previous commit.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_03_093256) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_05_123422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
