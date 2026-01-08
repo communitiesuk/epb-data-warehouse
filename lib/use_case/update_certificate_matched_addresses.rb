0d77d0da24726bea608c2a947177cd662de91386
Pedro Alvarez <pedro.alvarez@madetech.com>
2026-01-08 15:47:21 +0000

    OD-330: Add UpdateCertificateMatchedAddresses use case
    It will be used to consume matched_address_update
    redis queue.

---
+++ lib/use_case/update_certificate_matched_addresses.rb
@@ -0,0 +1,62 @@
+module UseCase
+  class UpdateCertificateMatchedAddresses
+    ASSESSMENT_ADDRESS_ID_KEY = "matched_uprn".freeze
+
+    def initialize(queues_gateway:, documents_gateway:, assessment_search_gateway:, assessments_address_id_gateway:, recovery_list_gateway:, logger: nil)
+      @queues_gateway = queues_gateway
+      @documents_gateway = documents_gateway
+      @assessment_search_gateway = assessment_search_gateway
+      @assessments_address_id_gateway = assessments_address_id_gateway
+      @recovery_list_gateway = recovery_list_gateway
+      @logger = logger
+      @queue_name = :matched_address_update
+    end
+
+    def execute(from_recovery_list: false)
+      if from_recovery_list
+        payload = @recovery_list_gateway.assessments queue: @queue_name
+      else
+        payload = @queues_gateway.consume_queue(@queue_name)
+        register_assessments_to_recovery_list payload
+      end
+
+      payload.each do |assessment|
+        payload_arr = assessment.split(":")
+        assessment_id = payload_arr[0]
+        matched_uprn = payload_arr[1]
+        if address_id_valid?(matched_uprn)
+          @documents_gateway.set_top_level_attribute assessment_id:, top_level_attribute: ASSESSMENT_ADDRESS_ID_KEY, new_value: matched_uprn
+          @assessment_search_gateway.update_uprn assessment_id:, new_value: matched_uprn, override: false
+          @assessments_address_id_gateway.insert_or_update_matched_uprn assessment_id:, matched_uprn: matched_uprn
+        end
+        clear_assessment_on_recovery_list payload: assessment
+      rescue StandardError => e
+        report_to_sentry e
+        @logger.error "Error of type #{e.class} when updating an EPC address using #{assessment}: '#{e.message}'" if @logger.respond_to?(:error)
+        register_attempt_to_recovery_list payload: assessment unless e.is_a?(Errors::ConnectionApiError)
+      end
+    rescue StandardError => e
+      report_to_sentry e
+      @logger.error "Error of type #{e.class} when updating EPC address_ids certificates: '#{e.message}'" if @logger.respond_to?(:error)
+    end
+
+  private
+
+    def address_id_valid?(matched_uprn)
+      only_digits_pattern = /\A\d+\z/
+      matched_uprn.match?(only_digits_pattern)
+    end
+
+    def clear_assessment_on_recovery_list(payload:)
+      @recovery_list_gateway.clear_assessment payload:, queue: @queue_name
+    end
+
+    def register_attempt_to_recovery_list(payload:)
+      @recovery_list_gateway.register_attempt payload:, queue: @queue_name
+    end
+
+    def register_assessments_to_recovery_list(assessment_ids)
+      @recovery_list_gateway.register_assessments(*assessment_ids, queue: @queue_name)
+    end
+  end
+end
---
+++ spec/unit/use_case/update_certificate_matched_addresses_spec.rb
@@ -0,0 +1,131 @@
+require "time"
+
+describe UseCase::UpdateCertificateMatchedAddresses do
+  subject(:use_case) do
+    described_class.new queues_gateway:,
+                        documents_gateway:,
+                        assessment_search_gateway:,
+                        assessments_address_id_gateway:,
+                        recovery_list_gateway:,
+                        logger:
+  end
+
+  let(:queue_name) do
+    :matched_address_update
+  end
+
+  let(:documents_gateway) do
+    documents_gateway = instance_double(Gateway::DocumentsGateway)
+    allow(documents_gateway).to receive(:set_top_level_attribute)
+    documents_gateway
+  end
+
+  let(:assessment_search_gateway) do
+    assessment_search_gateway = instance_double(Gateway::AssessmentSearchGateway)
+    allow(assessment_search_gateway).to receive(:update_uprn)
+    assessment_search_gateway
+  end
+
+  let(:assessments_address_id_gateway) do
+    assessments_address_id_gateway = instance_double(Gateway::AssessmentsAddressIdGateway)
+    allow(assessments_address_id_gateway).to receive(:insert_or_update_matched_uprn)
+    assessments_address_id_gateway
+  end
+
+  let(:queues_gateway) do
+    instance_double(Gateway::QueuesGateway)
+  end
+
+  let(:recovery_list_gateway) do
+    gateway = instance_double(Gateway::RecoveryListGateway)
+    allow(gateway).to receive(:clear_assessment)
+    allow(gateway).to receive(:register_attempt)
+    allow(gateway).to receive(:register_assessments)
+    gateway
+  end
+
+  let(:logger) do
+    logger = instance_double(Logger)
+    allow(logger).to receive(:error)
+    logger
+  end
+
+  let(:payload) do
+    %w[0000-0000-0000-0000-0000:4444444444 0000-0000-0000-0000-0001:8888888888 0000-0000-0000-0000-0003:9999999999 0000-0000-0000-0000-0004:none 0000-0000-0000-0000-0005:unknown]
+  end
+
+  context "when the queues gateway is functioning correctly" do
+    before do
+      allow(queues_gateway).to receive(:consume_queue).and_return(payload)
+    end
+
+    context "when processing updates" do
+      it "runs without errors" do
+        expect { use_case.execute }.not_to raise_error
+      end
+
+      it "fetches the correct queue" do
+        use_case.execute
+        expect(queues_gateway).to have_received(:consume_queue).with(queue_name).exactly(1).times
+      end
+
+      it "updates the relevant certificates in the document store" do
+        use_case.execute
+        expect(documents_gateway).to have_received(:set_top_level_attribute).exactly(3).times
+        expect(documents_gateway).to have_received(:set_top_level_attribute).with(assessment_id: "0000-0000-0000-0000-0000", top_level_attribute: "matched_uprn", new_value: "4444444444").exactly(1).times
+      end
+
+      it "updates the assessment search table" do
+        use_case.execute
+        expect(assessment_search_gateway).to have_received(:update_uprn).exactly(3).times
+        expect(assessment_search_gateway).to have_received(:update_uprn).with(
+          assessment_id: "0000-0000-0000-0000-0001",
+          new_value: "8888888888",
+          override: false,
+        ).exactly(1).times
+      end
+
+      it "updates the assessments address id table" do
+        use_case.execute
+        expect(assessments_address_id_gateway).to have_received(:insert_or_update_matched_uprn).exactly(3).times
+        expect(assessments_address_id_gateway).to have_received(:insert_or_update_matched_uprn).with(
+          assessment_id: "0000-0000-0000-0000-0001",
+          matched_uprn: "8888888888",
+        ).exactly(1).times
+      end
+
+      it "clears the assessments from the recovery list" do
+        use_case.execute
+        expect(recovery_list_gateway).to have_received(:clear_assessment).exactly(5).times
+      end
+    end
+  end
+
+  context "when the queues gateway is not functioning correctly" do
+    before do
+      allow(queues_gateway).to receive(:consume_queue).and_raise("bang!")
+    end
+
+    it "logs out an error containing the underlying error message" do
+      use_case.execute
+
+      expect(logger).to have_received(:error).with(include "bang!")
+    end
+  end
+
+  context "when assessments are being fetched from the recovery list" do
+    before do
+      allow(recovery_list_gateway).to receive(:assessments).with(queue: queue_name).and_return(payload)
+      use_case.execute from_recovery_list: true
+    end
+
+    it "sends updates for all three certificates from the recovery list" do
+      expect(documents_gateway).to have_received(:set_top_level_attribute).exactly(3).times
+      expect(assessment_search_gateway).to have_received(:update_uprn).exactly(3).times
+    end
+
+    it "does not register the assessments onto the recovery list" do
+      expect(recovery_list_gateway).not_to have_received(:register_assessments)
+    end
+  end
+end
