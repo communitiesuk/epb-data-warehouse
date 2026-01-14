cb030188a85426c114090130e0b72d42d72dfcc9
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2026-01-14 14:18:54 +0000

    Update ActiveRecord and ActiveSupport to v8.1
    Includes the change to db/schema.rb resulting from the update.

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_06_141909) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_06_141909) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
-  enable_extension "plpgsql"
   enable_extension "tablefunc"
 
   create_table "assessment_attribute_lookups", force: :cascade do |t|
     t.bigint "attribute_id", null: false
     t.bigint "lookup_id", null: false
-    t.string "type_of_assessment", null: false
     t.string "schema_version"
+    t.string "type_of_assessment", null: false
     t.index ["attribute_id", "lookup_id", "type_of_assessment", "schema_version"], name: "attribute_lookup_index", unique: true
     t.index ["lookup_id"], name: "index_assessment_attribute_lookups_on_lookup_id"
     t.index ["schema_version"], name: "index_assessment_attribute_lookups_on_schema_version"
@@ -28,11 +28,11 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_06_141909) do
   end
 
   create_table "assessment_attribute_values", primary_key: ["attribute_id", "assessment_id"], force: :cascade do |t|
-    t.bigint "attribute_id", null: false
     t.string "assessment_id", null: false
+    t.bigint "attribute_id", null: false
     t.string "attribute_value"
-    t.integer "attribute_value_int"
     t.float "attribute_value_float"
+    t.integer "attribute_value_int"
     t.jsonb "json"
     t.index ["assessment_id", "attribute_id"], name: "index_assessment_id_attribute_id_on_aav", unique: true
     t.index ["assessment_id"], name: "index_assessment_attribute_values_on_assessment_id"
@@ -49,8 +49,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_06_141909) do
 
   create_table "assessment_documents", primary_key: "assessment_id", id: :string, force: :cascade do |t|
     t.jsonb "document", null: false
-    t.datetime "warehouse_created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "warehouse_created_at", null: false
     t.index "((document ->> 'assessment_type'::text))", name: "index_document_assessment_type"
     t.index "((document ->> 'created_at'::text))", name: "index_document_created_at"
     t.index "((document ->> 'postcode'::text))", name: "index_document_postcode"
@@ -68,22 +68,22 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_06_141909) do
   end
 
   create_table "assessment_search", primary_key: ["assessment_id", "registration_date"], force: :cascade do |t|
-    t.string "assessment_id", null: false
+    t.string "address", limit: 500
     t.string "address_line_1"
     t.string "address_line_2"
     t.string "address_line_3"
     t.string "address_line_4"
+    t.string "assessment_address_id", limit: 30
+    t.string "assessment_id", null: false
+    t.string "assessment_type", limit: 8
+    t.string "constituency", limit: 45
+    t.string "council", limit: 40
+    t.datetime "created_at", precision: nil
+    t.string "current_energy_efficiency_band", limit: 2
+    t.integer "current_energy_efficiency_rating"
     t.string "post_town", limit: 100
     t.string "postcode", limit: 10
-    t.integer "current_energy_efficiency_rating"
-    t.string "current_energy_efficiency_band", limit: 2
-    t.string "council", limit: 40
-    t.string "constituency", limit: 45
-    t.string "assessment_address_id", limit: 30
-    t.string "address", limit: 500
     t.datetime "registration_date", precision: nil, null: false
-    t.string "assessment_type", limit: 8
-    t.datetime "created_at", precision: nil
     t.bigint "uprn"
     t.index ["address"], name: "index_assessment_search_on_address_trigram", opclass: :gin_trgm_ops, using: :gin
     t.index ["assessment_address_id"], name: "index_assessment_search_on_assessment_address_id"
@@ -116,17 +116,17 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_06_141909) do
   end
 
   create_table "countries", primary_key: "country_id", id: :integer, default: nil, force: :cascade do |t|
+    t.jsonb "address_base_country_code"
     t.string "country_code"
     t.string "country_name"
-    t.jsonb "address_base_country_code"
   end
 
   create_table "ons_postcode_directory", primary_key: "postcode", id: { type: :string, limit: 8 }, force: :cascade do |t|
     t.string "country_code", limit: 9, null: false
-    t.string "region_code", limit: 9, null: false
     t.string "local_authority_code", limit: 9, null: false
-    t.string "westminster_parliamentary_constituency_code", limit: 9, null: false
     t.jsonb "other_areas", null: false
+    t.string "region_code", limit: 9, null: false
+    t.string "westminster_parliamentary_constituency_code", limit: 9, null: false
     t.index ["country_code"], name: "index_ons_postcode_directory_on_country_code"
     t.index ["local_authority_code"], name: "index_ons_postcode_directory_on_local_authority_code"
     t.index ["region_code"], name: "index_ons_postcode_directory_on_region_code"
@@ -144,9 +144,9 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_06_141909) do
   end
 
   create_table "ons_postcode_directory_versions", id: :serial, force: :cascade do |t|
-    t.string "version_month", limit: 7, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "version_month", limit: 7, null: false
   end
 
   add_foreign_key "assessment_attribute_lookups", "assessment_attributes", column: "attribute_id", primary_key: "attribute_id"
