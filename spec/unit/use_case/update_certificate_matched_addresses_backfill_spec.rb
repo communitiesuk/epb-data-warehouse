50d23398fed934ce130d0e8ac3fb7edc3ca2bb59
Alice Lui <54395483+al01010@users.noreply.github.com>
2026-01-28 11:45:05 +0000

    OD-330: Separate out use case to update certificate matched uprn
    - one for lodgements and one for backfill

---
+++ spec/unit/use_case/update_certificate_matched_addresses_backfill_spec.rb
@@ -0,0 +1,97 @@
+require "time"
+
+describe UseCase::UpdateCertificateMatchedAddressesBackfill do
+  subject(:use_case) do
+    described_class.new queues_gateway:,
+                        documents_gateway:,
+                        assessment_search_gateway:,
+                        recovery_list_gateway:,
+                        logger:
+  end
+
+  let(:queue_name) do
+    :backfill_matched_address_update
+  end
+
+  let(:documents_gateway) do
+    documents_gateway = instance_double(Gateway::DocumentsGateway)
+    allow(documents_gateway).to receive(:check_id_exists?).and_return(true)
+    allow(documents_gateway).to receive(:set_top_level_attribute)
+    documents_gateway
+  end
+
+  let(:assessment_search_gateway) do
+    assessment_search_gateway = instance_double(Gateway::AssessmentSearchGateway)
+    allow(assessment_search_gateway).to receive(:update_uprn)
+    assessment_search_gateway
+  end
+
+  let(:queues_gateway) do
+    instance_double(Gateway::QueuesGateway)
+  end
+
+  let(:recovery_list_gateway) do
+    gateway = instance_double(Gateway::RecoveryListGateway)
+    allow(gateway).to receive(:clear_assessment)
+    allow(gateway).to receive(:register_attempt)
+    allow(gateway).to receive(:register_assessments)
+    gateway
+  end
+
+  let(:logger) do
+    logger = instance_double(Logger)
+    allow(logger).to receive(:error)
+    logger
+  end
+
+  let(:payload) do
+    %w[0000-0000-0000-0000-0000:4444444444 0000-0000-0000-0000-0001:8888888888 0000-0000-0000-0000-0003:9999999999 0000-0000-0000-0000-0004:none 0000-0000-0000-0000-0005:unknown]
+  end
+
+  context "when validating the queue name" do
+    let(:valid_queue_names) do
+      Gateway::QueueNames::QUEUE_NAMES
+    end
+
+    it "is is valid" do
+      expect(valid_queue_names.include?(queue_name)).to be true
+    end
+  end
+
+  context "when listening to the backfill queue" do
+    before do
+      allow(queues_gateway).to receive(:consume_queue).and_return(payload)
+    end
+
+    it "fetches the correct queue" do
+      use_case.execute
+      expect(queues_gateway).to have_received(:consume_queue).with(queue_name).exactly(1).times
+    end
+
+    it "updates the relevant certificates in the document store without updating 'updated_at' value" do
+      use_case.execute
+      expect(documents_gateway).to have_received(:set_top_level_attribute).exactly(3).times
+      expect(documents_gateway).to have_received(:set_top_level_attribute).with(
+        assessment_id: "0000-0000-0000-0000-0000",
+        top_level_attribute: "matched_uprn",
+        new_value: "4444444444",
+        update: false,
+      ).exactly(1).times
+    end
+
+    it "updates the assessment search table" do
+      use_case.execute
+      expect(assessment_search_gateway).to have_received(:update_uprn).exactly(3).times
+      expect(assessment_search_gateway).to have_received(:update_uprn).with(
+        assessment_id: "0000-0000-0000-0000-0001",
+        new_value: "8888888888",
+        override: false,
+      ).exactly(1).times
+    end
+
+    it "clears the assessments from the recovery list" do
+      use_case.execute
+      expect(recovery_list_gateway).to have_received(:clear_assessment).exactly(5).times
+    end
+  end
+end
--- spec/unit/use_case/update_certificate_matched_addresses_spec.rb
+++ spec/unit/use_case/update_certificate_matched_addresses_spec.rb
@@ -7,7 +7,6 @@ describe UseCase::UpdateCertificateMatchedAddresses do
                         documents_gateway:,
                         assessment_search_gateway:,
                         recovery_list_gateway:,
-                        queue_name: :matched_address_update,
                         logger:
   end
 
@@ -58,6 +57,16 @@ describe UseCase::UpdateCertificateMatchedAddresses do
 
   let(:payload) do
     %w[0000-0000-0000-0000-0000:4444444444 0000-0000-0000-0000-0001:8888888888 0000-0000-0000-0000-0003:9999999999 0000-0000-0000-0000-0004:none 0000-0000-0000-0000-0005:unknown]
+  end
+
+  context "when validating the queue name" do
+    let(:valid_queue_names) do
+      Gateway::QueueNames::QUEUE_NAMES
+    end
+
+    it "is is valid" do
+      expect(valid_queue_names.include?(queue_name)).to be true
+    end
   end
 
   context "when the queues gateway is functioning correctly" do
@@ -197,62 +206,6 @@ describe UseCase::UpdateCertificateMatchedAddresses do
 
     it "does not register the assessments onto the recovery list" do
       expect(recovery_list_gateway).not_to have_received(:register_assessments)
-    end
-  end
-
-  context "when listening to the backfill queue" do
-    subject(:use_case) do
-      described_class.new queues_gateway:,
-                          documents_gateway:,
-                          certificate_gateway:,
-                          assessment_search_gateway:,
-                          recovery_list_gateway:,
-                          queue_name: :backfill_matched_address_update,
-                          logger:
-    end
-
-    let(:queue_name) do
-      :backfill_matched_address_update
-    end
-
-    before do
-      allow(queues_gateway).to receive(:consume_queue).and_return(payload)
-    end
-
-    it "fetches the correct queue" do
-      use_case.execute
-      expect(queues_gateway).to have_received(:consume_queue).with(queue_name).exactly(1).times
-    end
-
-    it "does not fetch metadata for the assessment" do
-      use_case.execute
-      expect(certificate_gateway).not_to have_received(:fetch_meta_data)
-    end
-
-    it "updates the relevant certificates in the document store without updating 'updated_at' value" do
-      use_case.execute
-      expect(documents_gateway).to have_received(:set_top_level_attribute).exactly(3).times
-      expect(documents_gateway).to have_received(:set_top_level_attribute).with(
-        assessment_id: "0000-0000-0000-0000-0000",
-        top_level_attribute: "matched_uprn",
-        new_value: "4444444444",
-        update: false,
-      ).exactly(1).times
-    end
-
-    it "updates the assessment search table" do
-      use_case.execute
-      expect(assessment_search_gateway).to have_received(:update_uprn).exactly(3).times
-      expect(assessment_search_gateway).to have_received(:update_uprn).with(
-        assessment_id: "0000-0000-0000-0000-0001",
-        new_value: "8888888888",
-        override: false,
-      ).exactly(1).times
-    end
-
-    it "clears the assessments from the recovery list" do
-      use_case.execute
-      expect(recovery_list_gateway).to have_received(:clear_assessment).exactly(5).times
     end
   end
 end
